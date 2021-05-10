38ac8f2e8269a96ea38ef7b280a56ab126a68e88
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-05-10 18:10:30 -0400

    .

--- Validation_PET.R
+++ Validation_PET.R
@@ -3,27 +3,27 @@ cat("\014")
 
 library("hydroGOF")
 library("raster")
-library("xlsx")
 library("readxl")
 
-#Validation for GLEAM
+#Observations
 pet_shape<-shapefile("C:/Users/rooda/Dropbox/Patagonia/GIS South/Evapotranspiration_v10.shp")
 pet_obs<-as.data.frame(read_xlsx("C:/Users/rooda/Dropbox/Patagonia/Data/Evapotranspiration/Data_Evapotranspiration_v10.xlsx", sheet = "data_monthly", guess_max = 30000))
-pet_obs$Date <- NULL
+pet_obs$Date<-as.Date(pet_obs$Date)
 
-#Climate models (reanalysis, satellites, etc)
-pet_gleam<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Evapotranspiration/PET_GLEAM_1980_2020.nc", varname = "pet")
+#Gleam v3.5a
+pet_gleam<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Evapotranspiration/PET_GLEAM_1990_2019.nc", varname = "pet")
 pet_gleam <- pet_gleam[[which(getZ(pet_gleam) >= as.Date("2009-12-31"))]]
-pet_gleam<-setZ(pet_gleam, seq(as.Date("2010/1/1"), as.Date("2020/12/31"), "month"))
-
-#Extract
-pet_sim_gleam<-as.data.frame(t(extract(pet_gleam,pet_shape, method='simple')))
-colnames(pet_sim_gleam) <- pet_shape$Name
+pet_gleam<-setNames(pet_gleam, seq(as.Date("2010/1/1"), as.Date("2019/12/31"), "month"))
+pet_gleam<-as.data.frame(t(extract(pet_gleam, pet_shape, method='simple')))
+colnames(pet_gleam) <- pet_shape$Name
 
 #Performance
-KGE_pet_gleam<-KGE(sim=pet_sim_gleam, obs=pet_obs, method="2012", out.type="full",na.rm=TRUE)
-ME_pet_gleam<-me(sim=pet_sim_gleam, obs=pet_obs, na.rm=TRUE)
-rSD_pet_gleam<-rSD(sim=pet_sim_gleam, obs=pet_obs, na.rm=TRUE)
+pet_obs_subset<-subset(pet_obs, Date <= max(as.Date(rownames(pet_gleam), format =  "X%Y.%m.%d")))[,-1]
+
+KGE_pet_gleam<-KGE(sim=pet_gleam, obs=pet_obs_subset, method="2012", out.type="full",na.rm=TRUE)
+ME_pet_gleam<-me(sim=pet_gleam, obs=pet_obs_subset, na.rm=TRUE)
+rSD_pet_gleam<-rSD(sim=pet_gleam, obs=pet_obs_subset, na.rm=TRUE)
 
 #Save
-write.xlsx(cbind(t(KGE_pet_gleam$KGE.elements),KGE_pet_gleam$KGE.value, ME_pet_gleam, rSD_pet_gleam),"PET_Validation.xlsx")
+pet_validation<-cbind(t(KGE_pet_gleam$KGE.elements),KGE_pet_gleam$KGE.value, ME_pet_gleam, rSD_pet_gleam)
+write.csv(pet_validation,"C:/Users/rooda/Dropbox/Rstudio/PET_Validation.csv")
