b9717d38f8df906f9526d3c814b244c28caa4f8d
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-05-11 17:34:19 -0400

    added ERA5

--- TUWmodel_extraction.R
+++ TUWmodel_extraction.R
@@ -13,7 +13,7 @@ basins_int<-shapefile("C:/Users/rooda/Dropbox/Patagonia/GIS South/Basins_Patagon
 basins_int<-spTransform(basins_int, crs(dem_patagonia))
 basins_int<-basins_int[order(as.numeric(basins_int$gridcode)),]
 
-#PatagoniaMet v1.0, CR2MET v2.0 and MSWEP 2.8
+#PatagoniaMet v1.0, CR2MET v2.0, MSWEP 2.8 and ERA5d
 pp_pmet<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Precipitation/PP_PMET_1990_2019_v2.nc", varname = "PP")
 t2m_pmet<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_PMET_1990_2019.nc", varname = "tas")
 pet_gleam<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Evapotranspiration/PET_GLEAM_1990_2019.nc", varname = "pet")
@@ -29,10 +29,13 @@ pp_mswep<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Precipitation/PP_MSWEP_19
 pp_mswep<- setZ(pp_mswep, seq(as.Date('1979-02-01'), as.Date('2019-12-01'), by = 'month'))
 pp_mswep<-subset(pp_mswep, which(getZ(pp_mswep) >= '1990-01-01'))
 
-data_area<-matrix(0,83,7)
+pp_era5d<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Precipitation/PP_ERA5_1990_2019.nc", varname = "tp")
+t2m_era5d<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_ERA5_1990_2019_v1.nc", varname = "tas")
+
+data_area<-matrix(0,length(basins_int),7)
 data_area[,1]<-as.data.frame(read_xlsx("C:/Users/rooda/Dropbox/Patagonia/Data/streamflow/Data_streamflow_v10.xlsx", sheet = "info"))$Area_km2
 
-for (i in 1:83) {
+for (i in 1:length(basins_int)) {
   if (basins_int[i,]$Use == 1){
     
     elev_zones_i<-elevationZones(x=basins_int[i,], dem=dem_patagonia, max.zones = 5, min.elevZ = 300, elev.thres = NULL)
@@ -47,7 +50,10 @@ for (i in 1:83) {
       write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/PMET/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
       write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/CR2MET/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
       write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/MSWEP/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
+      write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/ERA5/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
       
+      t2m_i<-round(t(raster::extract(t2m_era5d, elev_zones_i, fun = mean)),3)
+      write.csv(t2m_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/ERA5/T2M/T2M_gridcode_", sprintf("%03d", i),".csv"))
       t2m_i<-round(t(raster::extract(t2m_pmet, elev_zones_i, fun = mean)),3)
       write.csv(t2m_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/PMET/T2M/T2M_gridcode_", sprintf("%03d", i),".csv"))
       t2m_i<-round(t(raster::extract(t2m_cr2met, elev_zones_i, fun = mean)),3)
@@ -60,6 +66,9 @@ for (i in 1:83) {
       write.csv(pp_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/CR2MET/PP/PP_gridcode_", sprintf("%03d", i),".csv"))
       pp_i<-round(t(raster::extract(pp_mswep, elev_zones_i, fun = mean)),0)
       write.csv(pp_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/MSWEP/PP/PP_gridcode_", sprintf("%03d", i),".csv"))
+      pp_i<-round(t(raster::extract(pp_era5d, elev_zones_i, fun = mean)),0)
+      write.csv(pp_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/ERA5/PP/PP_gridcode_", sprintf("%03d", i),".csv"))
+      
       
       print(i)
       
@@ -71,7 +80,10 @@ for (i in 1:83) {
       write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/PMET/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
       write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/CR2MET/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
       write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/MSWEP/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
+      write.csv(pet_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/ERA5/PET/PET_gridcode_", sprintf("%03d", i),".csv"))
       
+      t2m_i<-round(t(raster::extract(t2m_era5d, elev_zones_i, fun = mean)),3)
+      write.csv(t2m_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/ERA5/T2M/T2M_gridcode_", sprintf("%03d", i),".csv"))
       t2m_i<-round(t(raster::extract(t2m_pmet, elev_zones_i, fun = mean)),3)
       write.csv(t2m_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/PMET/T2M/T2M_gridcode_", sprintf("%03d", i),".csv"))
       t2m_i<-round(t(raster::extract(t2m_cr2met, elev_zones_i, fun = mean)),3)
@@ -84,6 +96,8 @@ for (i in 1:83) {
       write.csv(pp_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/CR2MET/PP/PP_gridcode_", sprintf("%03d", i),".csv"))
       pp_i<-round(t(raster::extract(pp_mswep, elev_zones_i, fun = mean)),0)
       write.csv(pp_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/MSWEP/PP/PP_gridcode_", sprintf("%03d", i),".csv"))
+      pp_i<-round(t(raster::extract(pp_era5d, elev_zones_i, fun = mean)),0)
+      write.csv(pp_i, paste0("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/ERA5/PP/PP_gridcode_", sprintf("%03d", i),".csv"))
       
       print(i)
     }
