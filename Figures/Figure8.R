ebe2e7fa7e5e50582961066f1cf85371a903fe34
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-07-06 10:43:33 -0400

    Change colors to mantain consistency

--- Figures/Figure8.R
+++ Figures/Figure8.R
@@ -18,7 +18,7 @@ title <-list(text = "a)", font = f, showarrow = F, xref = "paper", yref = "paper
 y <- list(title = "Correlation (r)", titlefont = f, 
           tickfont = f2, dtick = 0.2, ticks = "outside", zeroline = FALSE, range = c(0, 1))
 
-fig1 <- plot_ly(data_q, y = ~r, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(8, 'Paired'))
+fig1 <- plot_ly(data_q, y = ~r, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(4, 'Dark2'))
 fig1 <- fig1 %>% layout(xaxis = x, yaxis = y, showlegend = FALSE)
 fig1 <- fig1 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig1 <- fig1 %>% layout(annotations = title)
@@ -28,7 +28,7 @@ title2 <-list(text = "b)", font = f, showarrow = F, xref = "paper", yref = "pape
 y2 <- list(title = "Bias (β)", titlefont = f, range = c(0, 3),
            tickfont = f2, dtick = 1, ticks = "outside", zeroline = FALSE)
 
-fig2 <- plot_ly(data_q, y = ~Beta, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(8, 'Paired'))
+fig2 <- plot_ly(data_q, y = ~Beta, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(4, 'Dark2'))
 fig2 <- fig2 %>% layout(xaxis = x, yaxis = y2, showlegend = FALSE)
 fig2 <- fig2 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig2 <- fig2 %>% layout(annotations = title2)
@@ -38,7 +38,7 @@ title3 <-list(text = "c)", font = f, showarrow = F, xref = "paper", yref = "pape
 y3 <- list(title = "Variability (γ)", titlefont = f, range = c(0.3, 1.5),
            tickfont = f2, dtick = 0.3, ticks = "outside", zeroline = FALSE)
 
-fig3 <- plot_ly(data_q, y = ~Gamma, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(8, 'Paired'))
+fig3 <- plot_ly(data_q, y = ~Gamma, x = ~Model, type = "box", color = ~Model, colors =  brewer.pal(4, 'Dark2'))
 fig3 <- fig3 %>% layout(xaxis = x, yaxis = y3, showlegend = FALSE)
 fig3 <- fig3 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig3 <- fig3 %>% layout(annotations = title3)
@@ -47,7 +47,7 @@ title4 <-list(text = "d)", font = f, showarrow = F, xref = "paper", yref = "pape
 y4 <- list(title = "KGE", titlefont = f, 
            tickfont = f2, dtick = 0.5, ticks = "outside", zeroline = FALSE, range = c(-1, 1))
 
-fig4 <- plot_ly(data_q, y = ~KGE, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(8, 'Paired'))
+fig4 <- plot_ly(data_q, y = ~KGE, x = ~Model, type = "box", color = ~Model, colors =  brewer.pal(4, 'Dark2'))
 fig4 <- fig4 %>% layout(xaxis = x, yaxis = y4, showlegend = FALSE)
 fig4 <- fig4 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig4 <- fig4 %>% layout(annotations = title4)
