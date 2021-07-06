5fd9d15a307ab2e33640c9580510baf00620ad1e
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-07-06 10:46:51 -0400

    Calibration and validation periods

--- TUWmodel_model.R
+++ TUWmodel_model.R
@@ -10,8 +10,8 @@ q_obs<-as.data.frame(read_xlsx("C:/Users/rooda/Dropbox/Patagonia/Data/streamflow
 dirs<-list.dirs("C:/Users/rooda/Dropbox/Rstudio/TUWmodel", full.names = TRUE,recursive =FALSE)
 
 areas<-read.csv("C:/Users/rooda/Dropbox/Rstudio/TUWmodel/data_area.csv")
-KGE<-data.frame(matrix(0,sum(areas$nbands > 0)*4,5))
-colnames(KGE)<-c("Model", "KGE", "r", "Beta", "Gamma")
+KGE<-data.frame(matrix(ncol = 6, nrow = 1))
+colnames(KGE)<-c("Model", "Stage","KGE", "r", "Beta", "Gamma")
 models<-c("CR2MET","ERA5d", "MSWEP", "PMET")
 
 param = list(c("SCF", "DDF", "Tr", "Ts" ,  "Tm", "LPrat", "FC","BETA", "k0", "k1", "k2", "lsuz", "cperc", "bmax", "croute"),
@@ -21,9 +21,15 @@ for (j in 1:4) {
   for (i in 1:83) {
     if (areas$nbands[i] != 0){
       
-      q_obs_i<-subset(q_obs, Date > "1989-12-31")[,i+1]
-      q_obs_i<-q_obs_i*1000*24*3600*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)/(areas$area[i]*10^6)
+      q_obsc_i<-subset(q_obs, Date > "1989-12-31" & Date < "2005-12-31")[,i+1]
+      q_obsc_i<-q_obsc_i*1000*24*3600*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)/(areas$area[i]*10^6)
     
+      q_obsv_i<-subset(q_obs, Date > "2005-12-31")[,i+1]
+      q_obsv_i<-q_obsv_i*1000*24*3600*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)/(areas$area[i]*10^6)
+      
+      ok<-sum(is.na(q_obsc_i))/length(q_obsc_i)
+      if (ok <= 0.333){
+      
       pp_i<-paste0(dirs[j],"/PP/PP_gridcode_", sprintf("%03d", i), ".csv")
       t2m_i<-paste0(dirs[j],"/T2M/T2M_gridcode_", sprintf("%03d", i), ".csv")
       pet_i<-paste0(dirs[j],"/PET/PET_gridcode_", sprintf("%03d", i), ".csv")
@@ -34,36 +40,20 @@ for (j in 1:4) {
       tuw_model_q<-TUWmodel(prec = as.matrix(pp_i), airt= as.matrix(t2m_i), ep =  as.matrix(pet_i), 
                             area= areas[i,4:(areas$nbands[i]+3)], param=param[[2]], incon=c(100,10,150,150), itsteps=NULL)
       
-      if (areas$nbands[i] == 1){
+      KGE_cal_i<-KGE(sim=as.vector(tuw_model_q$q)[1:192], obs=q_obsc_i, method="2012", out.type="full",na.rm=TRUE)
+      KGE_val_i<-KGE(sim=as.vector(tuw_model_q$q)[193:360], obs=q_obsv_i, method="2012", out.type="full",na.rm=TRUE)
       
-      spin_off<-c(mean(matrix(tuw_model_q$moist, ncol = 12, byrow = TRUE)[5:30,1]), 
-                  mean(matrix(tuw_model_q$swe, ncol = 12, byrow = TRUE)[5:30,1]), 
-                  mean(matrix(tuw_model_q$suz, ncol = 12, byrow = TRUE)[5:30,1]), 
-                  mean(matrix(tuw_model_q$slz, ncol = 12, byrow = TRUE)[5:30,1]))
-      
-      } else {
-        
-      spin_off<-c(mean(matrix(tuw_model_q$moist[,1], ncol = 12, byrow = TRUE)[5:30,1]), 
-                    mean(matrix(tuw_model_q$swe[,1], ncol = 12, byrow = TRUE)[5:30,1]), 
-                    mean(matrix(tuw_model_q$suz[,1], ncol = 12, byrow = TRUE)[5:30,1]), 
-                    mean(matrix(tuw_model_q$slz[,1], ncol = 12, byrow = TRUE)[5:30,1]))
+      KGE<-rbind(KGE, c(models[j], "Calibration", KGE_cal_i$KGE.value, KGE_cal_i$KGE.elements))
+      KGE<-rbind(KGE, c(models[j], "Validation", KGE_val_i$KGE.value, KGE_val_i$KGE.elements))
       }
       
-      tuw_model_q<-TUWmodel(prec = as.matrix(pp_i), airt= as.matrix(t2m_i), ep =  as.matrix(pet_i), 
-                            area= areas[i,4:(areas$nbands[i]+3)], param=param[[2]], incon=spin_off, itsteps=NULL)
-      
-      KGE_i<-KGE(sim=as.vector(tuw_model_q$q), obs=q_obs_i, method="2012", out.type="full",na.rm=TRUE)
-      
-      KGE[which(KGE$Model == 0)[1],]<-c(models[j], KGE_i$KGE.value, KGE_i$KGE.elements)
       print(i)
       
-    } else {
-      print (i)
-      
-      }
-    }
-  print (j)
-}
+    } 
+    
+  }
   
-  write.csv(KGE, "C:/Users/rooda/Dropbox/Patagonia/MS1 Results/Q_performance.csv", row.names = FALSE)
+  }
+  
+write.csv(KGE, "C:/Users/rooda/Dropbox/Patagonia/MS1 Results/Q_performance.csv", row.names = FALSE)
   
