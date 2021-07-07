a9b7d406a8eb860bed61f1ebcfd925127a1646b5
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-07-07 09:48:41 -0400

    Rename

--- Figures/Figure8.R
+++ Figures/Figure8.R
@@ -7,8 +7,10 @@ library("RColorBrewer")
 
 Sys.setenv(PATH=paste0("C:/Users/rooda/AppData/Local/Programs/orca;", Sys.getenv("PATH")))
 
-data_q<-read.csv("C:/Users/rooda/Dropbox/Patagonia/MS1 Results/Q_performance.csv")
-data_q$Model <- factor(data_q$Model, levels = c("ERA5d - C", "ERA5d - V", "MSWEP - C", "MSWEP - V","CR2MET - C","CR2MET - V","PMET - C", "PMET - V"))
+data_pp<-read_xlsx("C:/Users/rooda/Dropbox/Patagonia/Figures/Figure7_Validation_v2.xlsx", sheet = "data_pp")
+data_t2m<-read_xlsx("C:/Users/rooda/Dropbox/Patagonia/Figures/Figure7_Validation_v2.xlsx", sheet = "data_t2m")
+data_pp$Model <- factor(data_pp$Model, levels = c("ERA5d", "MSWEP", "CR2MET","PMET"))
+data_t2m$Model <- factor(data_t2m$Model, levels = c("ERA5d", "MSWEP", "CR2MET","PMET"))
 
 f <- list(family = "Times New Roman", size = 22)
 f2 <- list(family = "Times New Roman", size = 18)
@@ -16,29 +18,30 @@ f2 <- list(family = "Times New Roman", size = 18)
 x <- list(titlefont = f, tickfont = f2, ticks = "outside")
 title <-list(text = "a)", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.01, y = 0.99)
 y <- list(title = "Correlation (r)", titlefont = f, 
-          tickfont = f2, dtick = 0.2, ticks = "outside", zeroline = FALSE, range = c(0, 1))
+          tickfont = f2, dtick = 0.2, ticks = "outside", zeroline = FALSE, range = c(0.2, 1))
 
-fig1 <- plot_ly(data_q, y = ~r, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(4, 'Dark2'))
+fig1 <- plot_ly(data_pp, y = ~r, x = ~Model, type = "box", 
+                color = ~Model, colors = brewer.pal(4, 'Dark2'))
 fig1 <- fig1 %>% layout(xaxis = x, yaxis = y, showlegend = FALSE)
 fig1 <- fig1 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig1 <- fig1 %>% layout(annotations = title)
-fig1
 
 title2 <-list(text = "b)", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.03, y = 0.99)
 y2 <- list(title = "Bias (β)", titlefont = f, range = c(0, 3),
            tickfont = f2, dtick = 1, ticks = "outside", zeroline = FALSE)
 
-fig2 <- plot_ly(data_q, y = ~Beta, x = ~Model, type = "box", color = ~Model, colors = brewer.pal(4, 'Dark2'))
+fig2 <- plot_ly(data_pp, y = ~Beta, x = ~Model, type = "box", 
+                color = ~Model, colors = brewer.pal(4, 'Dark2'))
 fig2 <- fig2 %>% layout(xaxis = x, yaxis = y2, showlegend = FALSE)
 fig2 <- fig2 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig2 <- fig2 %>% layout(annotations = title2)
-fig2
 
 title3 <-list(text = "c)", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.01, y = 0.95)
 y3 <- list(title = "Variability (γ)", titlefont = f, range = c(0.3, 1.5),
            tickfont = f2, dtick = 0.3, ticks = "outside", zeroline = FALSE)
 
-fig3 <- plot_ly(data_q, y = ~Gamma, x = ~Model, type = "box", color = ~Model, colors =  brewer.pal(4, 'Dark2'))
+fig3 <- plot_ly(data_pp, y = ~Gamma, x = ~Model, type = "box", 
+                color = ~Model, colors = brewer.pal(4, 'Dark2'))
 fig3 <- fig3 %>% layout(xaxis = x, yaxis = y3, showlegend = FALSE)
 fig3 <- fig3 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig3 <- fig3 %>% layout(annotations = title3)
@@ -47,16 +50,37 @@ title4 <-list(text = "d)", font = f, showarrow = F, xref = "paper", yref = "pape
 y4 <- list(title = "KGE", titlefont = f, 
            tickfont = f2, dtick = 0.5, ticks = "outside", zeroline = FALSE, range = c(-1, 1))
 
-fig4 <- plot_ly(data_q, y = ~KGE, x = ~Model, type = "box", color = ~Model, colors =  brewer.pal(4, 'Dark2'))
+fig4 <- plot_ly(data_pp, y = ~KGE, x = ~Model, type = "box", 
+                color = ~Model, colors = brewer.pal(4, 'Dark2'))
 fig4 <- fig4 %>% layout(xaxis = x, yaxis = y4, showlegend = FALSE)
 fig4 <- fig4 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
 fig4 <- fig4 %>% layout(annotations = title4)
 
-fig <- subplot(fig1, fig2,  fig3, fig4,nrows = 2, shareX = T, titleY = T, margin = c(0.04, 0.04, 0.01, 0.01))
+title5 <-list(text = "e)", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.01, y = 0.91)
+y5 <- list(title = "Mean error (β')", titlefont = f, 
+           tickfont = f2, dtick = 2, ticks = "outside", zeroline = FALSE, range = c(-4, 2))
+
+fig5 <- plot_ly(data_t2m, y = ~Beta2, x = ~Model, type = "box", 
+                color = ~Model, colors = brewer.pal(4, 'Dark2'))
+fig5 <- fig5 %>% layout(xaxis = x, yaxis = y5, showlegend = FALSE)
+fig5 <- fig5 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
+fig5 <- fig5 %>% layout(annotations = title5)
+
+title6 <-list(text = "f)", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.03, y = 0.91)
+y6 <- list(title = "Variability (γ')", titlefont = f, 
+           tickfont = f2, dtick = 0.1, ticks = "outside", zeroline = FALSE, range = c(0.8, 1.2))
+
+fig6 <- plot_ly(data_t2m, y = ~Gamma2, x = ~Model, type = "box", 
+                color = ~Model, colors = brewer.pal(4, 'Dark2'))
+fig6 <- fig6 %>% layout(xaxis = x, yaxis = y6, showlegend = FALSE)
+fig6 <- fig6 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
+fig6 <- fig6 %>% layout(annotations = title6)
+
+fig <- subplot(fig1, fig2, fig3, fig4, fig5, fig6, nrows = 3, shareX = T, titleY = T, margin = c(0.04, 0.04, 0.01, 0.01))
 fig
 
 server <- orca_serve()
-setwd("C:/Users/rooda/Dropbox/Patagonia/Figures/")
-server$export(fig, file = "Figure8_TUWmodel.pdf", width = 1200, height = 800, scale = 4)
-server$export(fig, file = "Figure8_TUWmodel.png", width = 1200, height = 800, scale = 4)
+server$export(fig, file = "Figure7_Validation_v2.pdf", width = 1200, height = 1000, scale = 4)
+server$export(fig, file = "Figure7_Validation_v2.png", width = 1200, height = 1000, scale = 4)
 server$close()
+
