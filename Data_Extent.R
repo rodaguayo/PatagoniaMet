7c37066ee76384da86a181332b7c624fbeaa900e
rodaguayo <roodaguayo@gmail.com>
2022-05-12 12:36:30 +0200

    use terra package instead of raster

--- Data_Extent.R
+++ Data_Extent.R
@@ -1,122 +1,117 @@
+# Code to change preprocess reanalysis datasets used in Aguayo et al. (in review)
+# Developed by Rodrigo Aguayo (2020-2022)
+
 rm(list=ls())
 cat("\014")  
 
-library("raster")
+library("terra")
 
-#Patagonia: ERA5
-cut<-extent(-79,-64,-57,-40)
+# Initialization of area and folder
+setwd("/home/rooda/Dropbox/Patagonia/Data/")
+cut<-ext(-79,-64,-57,-40)
 
-stack_pp<-stack("C:/Users/rooda/Desktop/PP_ERA5_1950_1978.nc", varname = "tp")
-stack_pp<-crop(stack_pp, cut)*c(31000, 28000, 31000, 30000, 31000, 30000, 31000, 31000, 30000, 31000, 30000, 31000)
-stack_pp <- setZ(stack_pp, seq(as.Date('1950-01-01'), as.Date('1978-12-31'), by = 'month'))
+# ERA5: Precipitation and air temperature (daily and monthly)
+stack_pp  <- rast(c("/home/rooda/Documents/PP_ERA5_1950_1978m.nc","/home/rooda/Documents/PP_ERA5_1979_2021m.nc"))
+stack_t2m <- rast(c("/home/rooda/Documents/T2M_ERA5_1950_1978m.nc","/home/rooda/Documents/T2M_ERA5_1979_2021m.nc"))
+stack_pp  <- round(crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*1000,0)
+stack_t2m <- round(crop(stack_t2m, cut)-273.15, 2)
+writeCDF(stack_pp, "Precipitation/PP_ERA5_1950_2021m.nc", overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m, "Temperature/T2M_ERA5_1950_2021m.nc", overwrite=TRUE, varname="t2m", unit="degC", longname="Temperature", zname="time", compression = 9)
 
-stack_t2m<-stack("C:/Users/rooda/Desktop/T2M_ERA5_1950_1978.nc", varname = "t2m")
-stack_t2m<-crop(stack_t2m, cut)-273.15
-stack_t2m <- setZ(stack_t2m, seq(as.Date('1950-01-01'), as.Date('1978-12-31'), by = 'month'))
+stack_pp      <- rast(c("/home/rooda/Documents/PP_ERA5_1950_1978d.nc","/home/rooda/Documents/PP_ERA5_1979_2021d.nc"))*1000
+stack_t2m_max <- rast(c("/home/rooda/Documents/T2M_ERA5_1950_1978d.nc","/home/rooda/Documents/T2M_ERA5_1979_2021d.nc"))-273.15
+stack_t2m_min <- rast(c("/home/rooda/Documents/T2M_ERA5_1950_1978d.nc","/home/rooda/Documents/T2M_ERA5_1979_2021d.nc"))-273.15
+stack_t2m_max <- tapp(stack_t2m_max, strftime(time(stack_t2m_max),format="%Y-%m-%d"), fun = max)
+stack_t2m_min <- tapp(stack_t2m_min, strftime(time(stack_t2m_min),format="%Y-%m-%d"), fun = min)
+stack_pp      <- tapp(stack_pp, strftime(time(stack_pp),format="%Y-%m-%d"), fun = mean)
+stack_pp      <- round(crop(stack_pp, cut) ,0)
+stack_t2m_max <- round(crop(stack_t2m, cut),2)
+stack_t2m_min <- round(crop(stack_t2m, cut),2)
+writeCDF(stack_pp,      "Precipitation/PP_ERA5_1950_2021d.nc", overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m_max, "Temperature/T2M_max_ERA5_1950_2021d.nc", overwrite=TRUE, varname="t2m_max", unit="degC", longname="Maximum temperature", zname="time", compression = 9)
+writeCDF(stack_t2m_min, "Temperature/T2M_min_ERA5_1950_2021d.nc", overwrite=TRUE, varname="t2m_min", unit="degC", longname="Minimum temperature", zname="time", compression = 9)
 
-stack_pp_post<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Precipitation/PP_ERA5_1979_2019.nc", varname = "Precipitation")
-stack_t2m_post<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_ERA5_1979_2019.nc", varname = "Temperature")
+# ERA5-land (ERA5L): Precipitation and air temperature (daily and monthly)
+stack_pp  <- rast("/home/rooda/Documents/PP_ERA5L_1950_2021m.nc")
+stack_t2m <- rast("/home/rooda/Documents/T2M_ERA5L_1950_2021m.nc")
+stack_pp  <- round(crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*1000,0)
+stack_t2m <- round(crop(stack_t2m, cut)-273.15,2)
+writeCDF(stack_pp, "Precipitation/PP_ERA5L_1950_2021m.nc", prec='integer', overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m, "Temperature/T2M_ERA5L_1950_2021m.nc", overwrite=TRUE, varname="t2m", unit="degC", longname="Temperature", zname="time", compression = 9)
 
-stack_pp<-stack(stack_pp, stack_pp_post)
-stack_t2m<-stack(stack_t2m, stack_t2m_post)
-stack_pp <- setZ(stack_pp, seq(as.Date('1950-01-01'), as.Date('2019-12-31'), by = 'month'))
-stack_t2m <- setZ(stack_t2m, seq(as.Date('1950-01-01'), as.Date('2019-12-31'), by = 'month'))
+stack_pp      <- rast(c("/home/rooda/Documents/PP_ERA5l_1950_1978d.nc","/home/rooda/Documents/PP_ERA5l_1979_2021d.nc"))*1000
+stack_t2m_max <- rast(c("/home/rooda/Documents/T2M_ERA5l_1950_1978d.nc","/home/rooda/Documents/T2M_ERA5l_1979_2021d.nc"))-273.15
+stack_t2m_min <- rast(c("/home/rooda/Documents/T2M_ERA5l_1950_1978d.nc","/home/rooda/Documents/T2M_ERA5l_1979_2021d.nc"))-273.15
+stack_t2m_max <- tapp(stack_t2m_max, strftime(time(stack_t2m_max),format="%Y-%m-%d"), fun = max)
+stack_t2m_min <- tapp(stack_t2m_min, strftime(time(stack_t2m_min),format="%Y-%m-%d"), fun = min)
+stack_pp      <- tapp(stack_pp, strftime(time(stack_pp),format="%Y-%m-%d"), fun = mean)
+stack_pp      <- round(crop(stack_pp, cut) ,0)
+stack_t2m_max <- round(crop(stack_t2m, cut),2)
+stack_t2m_min <- round(crop(stack_t2m, cut),2)
+writeCDF(stack_pp,      "Precipitation/PP_ERA5l_1950_2021d.nc", overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m_max, "Temperature/T2M_max_ERA5l_1950_2021d.nc", overwrite=TRUE, varname="t2m_max", unit="degC", longname="Maximum temperature", zname="time", compression = 9)
+writeCDF(stack_t2m_min, "Temperature/T2M_min_ERA5l_1950_2021d.nc", overwrite=TRUE, varname="t2m_min", unit="degC", longname="Minimum temperature", zname="time", compression = 9)
 
-writeRaster(stack_pp, "PP_ERA5_1950_2019.nc", format = "CDF", datatype='INT2S', overwrite=TRUE, varname="tp", varunit="mm", 
-            longname="precipitation", xname="X", yname="Y", zname="time", zunit="month")
+# MERRA2: Precipitation and air temperature (monthly)
+stack_pp  <- rast(list.files("C:/Users/rooda/Downloads/MERRA2/PP/", full.names = TRUE))
+stack_t2m <- rast(list.files("C:/Users/rooda/Downloads/MERRA2/T2M/", full.names = TRUE))
+stack_pp  <- round(crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*86400, 0)
+stack_t2m <- round(crop(stack_t2m, cut)-273.15, 2)
+stack_pp  <- setZ(stack_pp,  seq(as.POSIXct('1980-01-01'), as.POSIXct('2019-12-31'), by = 'month'))
+stack_t2m <- setZ(stack_t2m, seq(as.POSIXct('1980-01-01'), as.POSIXct('2019-12-31'), by = 'month'))
+writeCDF(stack_pp,  "Precipitation/PP_MERRA2_1980_2019m.nc", overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m, "Temperature/T2M_MERRA2_1980_2019m.nc", overwrite=TRUE, varname="t2m", unit="degC", longname="Temperature", zname="time", compression = 9)
 
-writeRaster(stack_t2m, "T2M_ERA5_1950_2019.nc", format = "CDF", overwrite=TRUE, varname="tas", varunit="degC", 
-            longname="temperature", xname="X", yname="Y", zname="time", zunit="month")
+# CSFR: Precipitation and air temperature (monthly)
+stack_pp  <- rast(c(list.files("C:/Users/rooda/Downloads/CSFR/pp/1979-2010/", full.names = TRUE)), list.files("C:/Users/rooda/Downloads/CSFR/pp/2011-2019/", full.names = TRUE))
+stack_t2m <- rast(c(list.files("C:/Users/rooda/Downloads/CSFR/t2m/1979-2010/", full.names = TRUE)), list.files("C:/Users/rooda/Downloads/CSFR/pp/2011-2019/", full.names = TRUE))
+stack_pp  <- rotate(stack_pp)
+stack_t2m <- rotate(stack_t2m)
+stack_pp  <- round(crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*4, 2)
+stack_t2m <- round(crop(stack_t2m, cut)-273.15, 2)
+stack_pp  <- setZ(stack_pp,  seq(as.POSIXct('1979-01-01'), as.POSIXct('2019-12-31'), by = 'month'))
+stack_t2m <- setZ(stack_t2m, seq(as.POSIXct('1979-01-01'), as.POSIXct('2019-12-31'), by = 'month'))
+writeCDF(stack_pp,  "Precipitation/PP_MERRA2_1979_2019m.nc", overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m, "Temperature/T2M_MERRA2_1979_2019m.nc",  overwrite=TRUE, varname="t2m", unit="degC", longname="Temperature", zname="time", compression = 9)
 
-#Patagonia: ERA5-land
-cut<-extent(-79,-64,-57,-40)
+# MSWEP v2.8: (monthly)
+stack_pp        <- rast(list.files("C:/Users/rooda/Downloads/MSWEP/", full.names = TRUE))
+stack_pp        <- crop(stack_pp, cut)
+time(stack_pp)  <- seq(as.POSIXct('1979-02-01'), as.POSIXct('2020-11-01'), by = 'month')
+writeCDF(stack_pp, "Precipitation/PP_MSWEPv28_1979_2021m.nc", prec='integer', overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
 
-stack_pp<-stack("C:/Users/rooda/Desktop/PP_ERA5-land_1981_2019.nc")
-stack_pp<-rotate(stack_pp)
-stack_pp_crop<-crop(stack_pp, cut)*c(31000, 28000, 31000, 30000, 31000, 30000, 31000, 31000, 30000, 31000, 30000, 31000)
-stack_pp_crop <- setZ(stack_pp_crop, seq(as.Date('1981-01-01'), as.Date('2019-12-31'), by = 'month'))
-writeRaster(stack_pp_crop, "pp_ERA5", format = "CDF", datatype='INT2S', overwrite=TRUE, varname="Precipitation", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time (Month)")
+#CR2MET v2.0 (daily and monthly)
+stack_pp      <- rast("/home/rooda/Documents/PP_CR2METv2_1979_2020d.nc")
+stack_t2m_max <- rast("/home/rooda/Documents/T2M_max_CR2METv2_1979_2020d.nc")
+stack_t2m_min <- rast("/home/rooda/Documents/T2M_min_CR2METv2_1979_2020d.nc")
+stack_pp      <- crop(stack_pp, cut)
+stack_t2m_max <- crop(stack_t2m_max, cut)
+stack_t2m_min <- crop(stack_t2m_min, cut)
+writeCDF(stack_pp,      "Precipitation/PP_CR2MET_1979_2020d.nc",    overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m_max, "Temperature/T2M_max_CR2MET_1979_2020d.nc", overwrite=TRUE, varname="t2m_max", unit="degC", longname="Maximum temperature", zname="time", compression = 9)
+writeCDF(stack_t2m_min, "Temperature/T2M_min_CR2MET_1979_2020d.nc", overwrite=TRUE, varname="t2m_min", unit="degC", longname="Minimum temperature", zname="time", compression = 9)
 
-stack_t2m<-stack("C:/Users/rooda/Desktop/T2M_ERA5-land_1981_2019.nc")
-stack_t2m<-rotate(stack_t2m)
-stack_t2m_crop<-crop(stack_t2m, cut)-273.15
-stack_t2m_crop<-round(stack_t2m_crop, 2)
-stack_t2m_crop <- setZ(stack_t2m_crop, seq(as.Date('1981-01-01'), as.Date('2019-12-31'), by = 'month'))
-writeRaster(stack_t2m_crop, "t2m_ERA5", format = "CDF",overwrite=TRUE, varname="Temperature", varunit="degC", xname="Longitude",   yname="Latitude", zname="Time", zunit = "Month")
+stack_t2m_max <- tapp(stack_t2m_max, strftime(time(stack_t2m_max),format="%Y-%m"), fun = mean)
+stack_t2m_min <- tapp(stack_t2m_min, strftime(time(stack_t2m_min),format="%Y-%m"), fun = mean)
+stack_pp      <- tapp(stack_t2m_min, strftime(time(stack_t2m_min),format="%Y-%m"), fun = sum)
+time(stack_t2m_max)  <- seq(as.POSIXct('1979-01-01'), as.POSIXct('2020-12-01'), by = 'month')
+time(stack_t2m_min)  <- seq(as.POSIXct('1979-01-01'), as.POSIXct('2020-12-01'), by = 'month')
+time(stack_pp)       <- seq(as.POSIXct('1979-01-01'), as.POSIXct('2020-12-01'), by = 'month')
+writeCDF(stack_pp,      "Precipitation/PP_CR2MET_1979_2020m.nc",    overwrite=TRUE, varname="pp", unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m_max, "Temperature/T2M_max_CR2MET_1979_2020m.nc", overwrite=TRUE, varname="t2m_max", unit="degC", longname="Maximum temperature", zname="time", compression = 9)
+writeCDF(stack_t2m_min, "Temperature/T2M_min_CR2MET_1979_2020m.nc", overwrite=TRUE, varname="t2m_min", unit="degC", longname="Minimum temperature", zname="time", compression = 9)
 
-#Patagonia: MERRA2
-cut<-extent(-79,-64,-57,-40)
-
-stack_pp<-stack(list.files("C:/Users/rooda/Downloads/MERRA2/PP/", full.names = TRUE))
-stack_pp_crop<-crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*86400
-stack_pp_crop <- setZ(stack_pp_crop, seq(as.Date('1980-01-01'), as.Date('2019-12-31'), by = 'month'))
-writeRaster(stack_pp_crop, "pp_MERRA2", format = "CDF", datatype='INT2S', overwrite=TRUE, varname="Precipitation", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time (Month)")
-
-stack_t2m<-stack(list.files("C:/Users/rooda/Downloads/MERRA2/T2M/", full.names = TRUE))
-stack_t2m_crop<-crop(stack_t2m, cut)-273.15
-stack_t2m_crop <- setZ(stack_t2m_crop, seq(as.Date('1980-01-01'), as.Date('2019-12-31'), by = 'month'))
-writeRaster(stack_t2m_crop, "t2m_MERRA2", format = "CDF", overwrite=TRUE, varname="Temperature", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time (Month)")
-
-#Patagonia: CSFR
-cut<-extent(-79,-64,-57,-40)
-
-stack_pp1<-stack(list.files("C:/Users/rooda/Downloads/CSFR/pp/1979-2010/", full.names = TRUE), varname="A_PCP_L1_AccumAvg")
-stack_pp2<-stack(list.files("C:/Users/rooda/Downloads/CSFR/pp/2011-2019/", full.names = TRUE), varname="A_PCP_L1_AccumAvg")
-stack_pp<-stack(stack_pp1, stack_pp2)
-stack_pp<-rotate(stack_pp)
-
-stack_pp_crop<-crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*4
-stack_pp_crop <- setZ(stack_pp_crop, seq(as.Date('1979-01-01'), as.Date('2019-12-31'), by = 'month'))
-writeRaster(stack_pp_crop, "pp_CSFR", format = "CDF", datatype='INT2S', overwrite=TRUE, varname="Precipitation", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time (Month)")
-
-stack_t2m1<-stack(list.files("C:/Users/rooda/Downloads/CSFR/t2m/1979-2010/", full.names = TRUE), varname="TMP_L103_Avg")
-stack_t2m2<-stack(list.files("C:/Users/rooda/Downloads/CSFR/t2m/2011-2019/", full.names = TRUE), varname="TMP_L103_Avg")
-stack_t2m2<-resample(stack_t2m2,stack_t2m1, method="bilinear")
-stack_t2m<-stack(stack_t2m1, stack_t2m2)
-stack_t2m_crop<-round(stack_t2m_crop, 2)
-stack_t2m<-rotate(stack_t2m)
-
-stack_t2m_crop<-crop(stack_t2m, cut)-273.15
-stack_t2m_crop<-round(stack_t2m_crop, 2)
-stack_t2m_crop <- setZ(stack_t2m_crop, seq(as.Date('1979-01-01'), as.Date('2019-12-31'), by = 'month'))
-writeRaster(stack_t2m_crop, "t2m_CSFR", format = "CDF", overwrite=TRUE, varname="Temperature", varunit="degC", xname="Longitude",   yname="Latitude", zname="Time(Month)")
-
-#Patagonia: MSWEP v2.8
-stack_pp<-stack(list.files("C:/Users/rooda/Downloads/MSWEP/", full.names = TRUE), varname="precipitation")
-stack_pp<-crop(stack_pp, cut)
-stack_pp<- setZ(stack_pp, seq(as.Date('1979-02-01'), as.Date('2020-11-01'), by = 'month'))
-stack_pp<-subset(stack_pp, which(getZ(stack_pp) <= '2019-12-31'))
-stack_pp<- setZ(stack_pp, seq(as.Date('1979-02-01'), as.Date('2019-12-01'), by = 'month'))
-writeRaster(stack_pp, "PP_MSWEP_1979_2019", format = "CDF", datatype='INT2S', overwrite=TRUE, varname="Precipitation", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time (Month)")
-
-#Patagonia: CR2MET v2.0
-cut<-extent(-79,-64,-57,-40)
-
-stack_pp<-stack("E:/Datasets/CR2MET/CR2MET_pr_v2.0_mon_1979_2019_005deg.nc", varname="pr")
-stack_pp<-crop(stack_pp, cut)
-stack_pp<- setZ(stack_pp, seq(as.Date('1979-01-01'), as.Date('2019-12-01'), by = 'month'))
-writeRaster(stack_pp, "PP_CR2MET_1979_2019.nc", format = "CDF", datatype='INT2S', overwrite=TRUE, varname="Precipitation", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time (Month)")
-
-stack_pp<-stack("E:/Datasets/CR2MET/CR2MET_t2m_v2.0_mon_1979_2019_005deg.nc", varname="t2m")
-stack_pp<-crop(stack_pp, cut)
-stack_pp<- setZ(stack_pp, seq(as.Date('1979-01-01'), as.Date('2019-12-01'), by = 'month'))
-writeRaster(stack_pp, "T2M_CR2MET_1979_2019.nc", format = "CDF", overwrite=TRUE, varname="Temperature", varunit="degC", xname="Longitude",   yname="Latitude", zname="Time (Month)")
-
-#Patagonia: CR2-RegCM4
-cut<-extent(-76,-66,-57,-40)
-
-stack_pp<-stack(list.files("C:/Users/rooda/Desktop/pr/", full.names = TRUE), varname="pr")
-stack_pp_crop<-crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*86400
-stack_pp_crop<- setZ(stack_pp_crop, seq(as.Date('1980-01-01'), as.Date('2015-12-31'), by = 'month'))
-writeRaster(stack_pp_crop, "PP_CR2MET_RegCM4_1980_2015.nc", format = "CDF", datatype='INT2S', overwrite=TRUE, varname="Precipitation", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time (Month)")
-
-stack_t2m<-stack(list.files("C:/Users/rooda/Desktop/tas/", full.names = TRUE), varname="tas")
-stack_t2m_crop<-crop(stack_t2m, cut)-273.15
-stack_t2m_crop <- setZ(stack_t2m_crop, seq(as.Date('1980-01-01'), as.Date('2015-12-31'), by = 'month'))
-writeRaster(stack_t2m_crop, "T2M_CR2MET_RegCM4__1980_2015.nc", format = "CDF", overwrite=TRUE, varname="tas", varunit="degC", xname="Longitude",   yname="Latitude", zname="Time(Month)")
+# RegCM4-CR2 (monthly)
+stack_pp  <- rast(list.files("C:/Users/rooda/Desktop/pr/", full.names = TRUE))
+stack_t2m <- rast(list.files("C:/Users/rooda/Desktop/tas/", full.names = TRUE))
+stack_pp  <- crop(stack_pp, cut)*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)*86400
+stack_t2m <- crop(stack_t2m, cut)-273.15
+stack_pp  <- setZ(stack_pp,  seq(as.POSIXct('1980-01-01'), as.POSIXct('2015-12-31'), by = 'month'))
+stack_t2m <- setZ(stack_t2m, seq(as.POSIXct('1980-01-01'), as.POSIXct('2015-12-31'), by = 'month'))
+writeCDF(stack_pp,  "Precipitation/PP_MERRA2_1980_2015m.nc", overwrite=TRUE, varname="pp",  unit="mm", longname="Precipitation", zname="time", compression = 9)
+writeCDF(stack_t2m, "Temperature/T2M_MERRA2_1980_2015m.nc",  overwrite=TRUE, varname="t2m", unit="degC", longname="Temperature", zname="time", compression = 9)
 
 #Patagonia: PET-ET Gleam
-cut<-extent(-76,-66,-57,-40)
 fill.na <- function(x, i=5) {if( is.na(x)[i] ) {return(mean(x, na.rm=TRUE))} else {return(x[i])}}  
 
 pet_gleam<-flip(t(stack("C:/Users/rooda/Downloads/Ep_1980-2020_GLEAM_v3.5a_MO.nc", varname = "Ep")), 1)
@@ -130,19 +125,6 @@ pet_gleam_mean<-mean(stackApply(pet_gleam, indices<-format(pet_gleam@z$time,"%y"
 pet_gleam_mean[pet_gleam_mean == 0] <- NA
 writeRaster(pet_gleam, "PET_GLEAM_1990_2019.nc", datatype = "INT2S" ,format = "CDF", overwrite=TRUE, varname="pet", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time(Month)")
 writeRaster(pet_gleam_mean, "PET_GLEAM_1990_2019_mean.tif", format = "GTiff", overwrite = TRUE)
-
-et_gleam<-flip(t(stack("C:/Users/rooda/Downloads/E_1980-2020_GLEAM_v3.5a_MO.nc", varname = "E")), 1)
-crs(et_gleam)<-CRS("+init=epsg:4326")
-et_gleam<-crop(et_gleam, cut)
-
-for(i in 1:492) {et_gleam[[i]]<- focal(et_gleam[[i]], w = matrix(1,3,3), fun = fill.na, pad = TRUE, na.rm = FALSE)}
-et_gleam<-setZ(et_gleam, seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "month"))
-et_gleam<-subset(et_gleam, which(getZ(et_gleam) >= '1990-01-01' & (getZ(et_gleam) <= '2019-12-31')))
-et_gleam_mean<-mean(stackApply(et_gleam, indices<-format(et_gleam@z$time,"%y"), fun=sum))
-et_gleam_mean[et_gleam_mean == 0] <- NA
-writeRaster(et_gleam, "ET_GLEAM_1990_2019.nc", datatype = "INT2S" ,format = "CDF", overwrite=TRUE, varname="pet", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time(Month)")
-writeRaster(et_gleam_mean, "ET_GLEAM_1990_2019_mean.tif", format = "GTiff", overwrite = TRUE)
-
 
 #BH-DGA Stage III and IV
 
@@ -187,7 +169,7 @@ rownames(basins_pp)<-basins$gridcode
 rownames(basins_pet)<-basins$gridcode
 rownames(basins_et)<-basins$gridcode
 
-write.xlsx(cbind(basins$gridcode,basins_pp, basins_pet, basins_et), "eva_aridity_bh3.xlsx")
+write.csv(cbind(basins$gridcode,basins_pp, basins_pet, basins_et), "eva_aridity_bh3.csv")
 
 stack_pp4<- raster("E:/Datasets/DGA_BH/BH4/4_Base_de_datos/Archivos_raster/BH_85-15/Forzantes/1_Historico/pr_Anual_LatLon.tif")
 stack_pet4<-raster("E:/Datasets/DGA_BH/BH4/4_Base_de_datos/Archivos_raster/BH_85-15/VIC/1_Historico/pet_Anual_LatLon.tif")
@@ -200,8 +182,4 @@ rownames(basins_pp)<-basins$gridcode
 rownames(basins_pet)<-basins$gridcode
 rownames(basins_et)<-basins$gridcode
 
-write.xlsx(cbind(basins$gridcode,basins_pp, basins_pet, basins_et), "eva_aridity_bh4.xlsx")
-
-
-
-
+write.csv(cbind(basins$gridcode,basins_pp, basins_pet, basins_et), "eva_aridity_bh4.csv")
