eafa4f228cdbe7d328b80858247f9b7605cbb30f
rodaguayo <roodaguayo@gmail.com>
2022-05-20 15:06:03 +0200

    easy to read and replicate

--- TUWmodel_calibration.R
+++ TUWmodel_calibration.R
@@ -4,19 +4,20 @@ cat("\014")
 library("TUWmodel")
 library("hydroGOF")
 library("hydroPSO")
-library("readxl")
-library("raster")
+
+setwd("/home/rooda/Dropbox/Patagonia/")
 
 #Warm up, calibration and validation period 
 cal_period <- c("1992-12-31", "2006-12-31")
 val_period <- c("2007-01-01", "2019-12-31")
-dates<-seq(as.Date("1990-01-01"), as.Date("2019-12-31"), by ="month")
+dates      <- seq(as.Date("1990-01-01"), as.Date("2019-12-31"), by ="month")
 
 #General setting
-q_obs<-as.data.frame(read_xlsx("/home/rooda/Dropbox/Patagonia/Data/Streamflow/Data_Streamflow_v10.xlsx", sheet = "data_monthly"))
-q_obs<-subset(q_obs, Date >= "1989-12-31")
-areas<-read.csv("/home/rooda/Dropbox/Rstudio/TUWmodel/data_area.csv")
-dirs<-list.dirs("/home/rooda/Dropbox/Rstudio/TUWmodel", full.names = TRUE, recursive =FALSE)[1:4]
+days  <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+q_obs <- read.csv("Data/Streamflow/Data_Streamflow_v10_monthly.csv")
+q_obs <- subset(q_obs, Date >= "1989-12-31")
+areas <- read.csv("MS1 Results/TUWmodel/data_area.csv")
+dirs  <- list.dirs("MS1 Results/TUWmodel", full.names = TRUE, recursive =FALSE)[1:4]
 
 #Parameters: Lower and upper bound
 names_param        <-  c("SCF", "DDF", "Tr",  "Ts", "Tm", "LPrat",  "FC","Beta", "k0", "k1", "k2", "lsuz", "cperc", "bmax", "croute")
@@ -28,8 +29,8 @@ names(lower_param) <-  names_param
 names(ave_params)  <-  names_param
 
 #Perfomance metric (KGE 2012)
-KGE_params<-data.frame(matrix(ncol = 7+15, nrow = 55*4))
-colnames(KGE_params)<-c("Name", "Model", "Stage","KGE", "r", "Beta", "Gamma", names_param)
+KGE_params <- data.frame(matrix(ncol = 7+length(names_param), nrow = 55*4))
+colnames(KGE_params) <- c("Name", "Model", "Stage","KGE", "r", "Beta", "Gamma", names_param)
 seq<-seq(1,55*2,2)
 q_sim_final<-q_obs
 
@@ -49,22 +50,22 @@ TUWhydromod <- function(param.values, obs=q_obsc_i, PP=pp_i, T2M=t2m_i, PET=pet_
 
 #test<-TUWhydromod(param.values = ave_params, obs=q_obsc_i, PP=pp_i, T2M=t2m_i, PET=pet_i, AREA=area_m2, INITIAL = t0_cond)
 
-for (j in 1:4) {
-  for (i in 1:83) {
-    if (areas$nbands[i] != 0){
+for (j in 1:4) { #number of scenarios
+  for (i in 1:(length(q_obs)-1)) { #total number of basins
+    if (areas$nbands[i] != 0){ 
       
-      q_obs_i<-q_obs[,i+1]*1000*24*3600*c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)/(areas$area[i]*10^6)
-      q_obsc_i<-subset(q_obs_i, dates >= cal_period[1] & dates < cal_period[2])
-      q_obsv_i<-subset(q_obs_i, dates > cal_period[2])
+      q_obs_i  <- q_obs[,i+1]*1000*86400*days/(areas$area[i]*10^6)
+      q_obsc_i <- subset(q_obs_i, dates >= cal_period[1] & dates < cal_period[2])
+      q_obsv_i <- subset(q_obs_i, dates > cal_period[2])
       
-      pp_i<-paste0(dirs[j],"/PP/PP_gridcode_", sprintf("%03d", i), ".csv")
-      t2m_i<-paste0(dirs[j],"/T2M/T2M_gridcode_", sprintf("%03d", i), ".csv")
-      pet_i<-paste0(dirs[j],"/PET/PET_gridcode_", sprintf("%03d", i), ".csv")
-      pp_i<-read.table(pp_i, sep = ",", row.names=1, header = TRUE)
-      t2m_i<-read.table(t2m_i, sep = ",", row.names=1, header = TRUE)
-      pet_i<-read.table(pet_i, sep = ",", row.names=1, header = TRUE)
-      area_m2<-areas[i,4:(areas$nbands[i]+3)]
-      t0_cond   <- 1.5*matrix(mean(q_obs_i[seq(1, length(q_obs_i), 12)], na.rm = TRUE),4,areas$nbands[i])
+      pp_i    <- paste0(dirs[j],"/PP/PP_gridcode_",   sprintf("%03d", i), ".csv")
+      t2m_i   <- paste0(dirs[j],"/T2M/T2M_gridcode_", sprintf("%03d", i), ".csv")
+      pet_i   <- paste0(dirs[j],"/PET/PET_gridcode_", sprintf("%03d", i), ".csv")
+      pp_i    <- read.table(pp_i,  sep = ",", row.names=1, header = TRUE)
+      t2m_i   <- read.table(t2m_i, sep = ",", row.names=1, header = TRUE)
+      pet_i   <- read.table(pet_i, sep = ",", row.names=1, header = TRUE)
+      area_m2 <- areas[i,4:(areas$nbands[i]+3)]
+      t0_cond <- 1.5*matrix(mean(q_obs_i[seq(1, length(q_obs_i), 12)], na.rm = TRUE),4,areas$nbands[i])
       
       #out <- hydroPSO(fn="hydromodInR", lower=lower_param,  upper=upper_param, method="spso2007", model.FUN="TUWhydromod",
       #          control=list(write2disk=FALSE, MinMax="max", maxit=200, normalise=TRUE, REPORT=5, reltol= 1E-6, parallel = "parallel"),
@@ -74,15 +75,15 @@ for (j in 1:4) {
                       control=list(write2disk=FALSE, MinMax="max", npart=100, maxit=100, normalise=TRUE, REPORT=5, reltol=1E-6, parallel = "parallel"),
                       model.FUN.args= list(obs=q_obsc_i, PP=pp_i, T2M=t2m_i, PET=pet_i, AREA=area_m2, INITIAL= t0_cond))
       
-      tuw_model_q<-TUWmodel(prec = as.matrix(pp_i), airt= as.matrix(t2m_i), ep =  as.matrix(pet_i), 
+      tuw_model_q  <- TUWmodel(prec = as.matrix(pp_i), airt= as.matrix(t2m_i), ep =  as.matrix(pet_i), 
                             area= area_m2, param=out$par, itsteps=NULL, incon=t0_cond)
       
       q_sim_final[,i+1] <- as.numeric(tuw_model_q$q)
       q_sim_cal         <- subset(as.numeric(tuw_model_q$q), dates >= cal_period[1] & dates < cal_period[2])
       q_sim_val         <- subset(as.numeric(tuw_model_q$q), dates > cal_period[2])
       
-      KGE_cal_i<-KGE(sim=as.vector(q_sim_cal), obs=q_obsc_i, method="2012", out.type="full", na.rm=TRUE)
-      KGE_val_i<-KGE(sim=as.vector(q_sim_val), obs=q_obsv_i, method="2012", out.type="full", na.rm=TRUE)
+      KGE_cal_i <- KGE(sim=as.vector(q_sim_cal), obs=q_obsc_i, method="2012", out.type="full", na.rm=TRUE)
+      KGE_val_i <- KGE(sim=as.vector(q_sim_val), obs=q_obsv_i, method="2012", out.type="full", na.rm=TRUE)
       
       index<-sum(areas$nbands[1:i]>0)
       KGE_params[seq[index]+110*(j-1),]  <-c(colnames(q_obs)[i+1],colnames(q_obsc_i),basename(dirs)[j], "Calibration", KGE_cal_i$KGE.value, KGE_cal_i$KGE.elements, out$par)
@@ -96,6 +97,6 @@ for (j in 1:4) {
     }
 }
 
-file<-"/home/rooda/Dropbox/Patagonia/MS1 Results/Q_performance.csv"
-ifelse(file.exists(file), file.rename(file, "/home/rooda/Dropbox/Patagonia/MS1 Results/Q_performance_old.csv", overwrite = TRUE))
+file<-"MS1 Results/Q_performance.csv"
+ifelse(file.exists(file), file.rename(file, "MS1 Results/Q_performance_old.csv", overwrite = TRUE))
 write.csv(KGE_params, file, row.names = FALSE)
--- Validation_T2M.R
+++ Validation_T2M.R
@@ -10,30 +10,27 @@ library("terra")
 setwd("/home/rooda/Dropbox/Patagonia/Data/Temperature/")
 
 #Observations (location and data)
-t2m_shape <- read.csv("Metadata_Temperature/v10.csv")
-t2m_shape <- vect(t2m_shape, geom=c("Longitude", "Latitude"), crs="epsg:4326")
-t2m_obs   <- read.csv("Data_Temperature_v10_monthly.csv")
+t2m_data <- read.csv("Metadata_Temperature_v10.csv")
+t2m_shape <- vect(t2m_data, geom=c("Longitude", "Latitude"), crs="epsg:4326")
+t2m_obs   <- read.csv("Data_Temperature_mean_v10_monthly.csv")
 t2m_obs$Date<-as.Date(t2m_obs$Date) #The date is the first column
 
 #Climate models (reanalysis, satellites, etc)
-t2m_era5<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_ERA5_1950_2019.nc", varname = "tas")
-t2m_era5_v2<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_ERA5_1990_2019_v1.nc", varname = "tas")
+t2m_era5    <- rast("T2M_ERA5_1950_2019.nc")
 
-t2m_merra2<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_MERRA2_1980_2019.nc")
-t2m_merra2<-setNames(t2m_merra2,seq(as.Date("1980/1/1"), as.Date("2019/12/1"), "month"))
 
-t2m_csfr<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_CSFR_1979_2019.nc")
-t2m_csfr<-setNames(t2m_csfr,seq(as.Date("1979/1/1"), as.Date("2019/12/1"), "month"))
+t2m_era5_v2 <- rast("T2M_ERA5_1990_2019_v1.nc")
 
-t2m_cr2reg<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_CR2MET_RegCM4__1980_2015.nc")
-t2m_cr2reg<-setNames(t2m_cr2reg,seq(as.Date("1980/1/1"), as.Date("2015/12/1"), "month"))
+t2m_merra2 < -rast("T2M_MERRA2_1980_2019.nc")
 
-t2m_cr2met<-stack("E:/Datasets/CR2MET/T2M_CR2MET_1979_2019.nc")
-t2m_cr2met<-setNames(t2m_cr2met,seq(as.Date("1979/1/1"), as.Date("2019/12/1"), "month"))
-t2m_cr2met<-setZ(t2m_cr2met,seq(as.Date("1979/1/1"), as.Date("2019/12/1"), "month"))
-t2m_cr2met<-subset(t2m_cr2met, which(getZ(t2m_cr2met) >= '1990-01-01' & (getZ(t2m_cr2met) <= '2019-12-31')))
+t2m_csfr <- rast("T2M_CSFR_1979_2019.nc")
 
-t2m_pmet<-stack("C:/Users/rooda/Dropbox/Patagonia/Data/Temperature/T2M_PMET_1990_2019.nc", varname = "tas")
+t2m_cr2reg <- stack("T2M_CR2MET_RegCM4__1980_2015.nc")
+
+t2m_cr2met <- stack("T2M_CR2MET_1979_2019.nc")
+t2m_cr2met <- subset(t2m_cr2met, which(getZ(t2m_cr2met) >= '1990-01-01' & (getZ(t2m_cr2met) <= '2019-12-31')))
+
+t2m_pmet <- stack("T2M_PMET_1990_2019.nc", varname = "tas")
 
 #Extract
 t2m_era5<-t(extract(t2m_era5,t2m_shape, method='simple'))
@@ -44,13 +41,6 @@ t2m_cr2reg<-t(extract(t2m_cr2reg,t2m_shape, method='simple'))
 t2m_cr2met<-t(extract(t2m_cr2met,t2m_shape, method='simple'))
 t2m_cr2met[is.na(t2m_cr2met)] <- -9999
 t2m_pmet<-t(extract(t2m_pmet,t2m_shape, method='simple'))
-colnames(t2m_era5) <- t2m_shape$ID
-colnames(t2m_era5_v2) <- t2m_shape$ID
-colnames(t2m_merra2) <- t2m_shape$ID
-colnames(t2m_csfr) <- t2m_shape$ID
-colnames(t2m_cr2reg) <- t2m_shape$ID
-colnames(t2m_cr2met) <- t2m_shape$ID
-colnames(t2m_pmet) <- t2m_shape$ID
 
 #Subset and performance (CR2MET, ERA5v2 and PMET: 1990-2019)
 t2m_obs_subset<-subset(t2m_obs, Date >= min(as.Date(rownames(t2m_era5), format =  "X%Y.%m.%d")))[,-1]
