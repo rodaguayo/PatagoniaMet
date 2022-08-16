706fe44a44f09370c275acdf7018921918ea4a4e
rodaguayo <roodaguayo@gmail.com>
2022-08-16 10:15:11 +0200

    New code to process predictors for RF regression models

---
+++ Covariables_RF.R
@@ -0,0 +1,62 @@
+# Code for precipitation bias correction
+# Developed by Rodrigo Aguayo (2020-2022)
+
+# Code for temperature bias correction: Variance and mean scaling ---------------------------------
+# Developed by Rodrigo Aguayo (2020-2022)
+
+rm(list=ls())
+cat("\014")  
+
+library("terra")
+
+setwd("/home/rooda/Dropbox/Patagonia/")
+
+# Sample dataset
+sample <- rast(extent = ext(c(-75.75, -68.25, -55.75, -40.5)), crs = "+init=epsg:4326", resolution = 0.05)
+
+# Elevation from NASADEM 90m
+dem <- rast("GIS South/dem_patagonia3f.tif")
+dem[is.na(dem)] <- 0
+dem <- resample(dem, sample)
+#dem[dem <= 0] <- NA
+writeRaster(dem, "GIS South/dem_patagonia005.tif", overwrite=TRUE)
+
+# Aspect from NASADEM 90m
+aspect  <- terrain(dem, v='aspect', unit='degrees')
+writeRaster(aspect, "GIS South/aspect_dem005.tif", overwrite=TRUE)
+
+# Distance to coast from NASADEM 90m
+distance <- rast("GIS South/dem_patagonia3f.tif")
+distance <- resample(distance, sample)
+distance[is.na(distance)] <- -1
+distance[distance > 0] <- 1
+distance[distance == 1] <- NA
+distance <- distance(distance)
+distance <- log10(distance+1)
+writeRaster(distance, "GIS South/dist_coast005l.tif", overwrite=TRUE)
+
+# West gradient from NASADEM 90m
+gradient <- rast("GIS South/west_gradient_raw.tif")
+gradient <- resample(gradient, sample)
+writeRaster(gradient, "GIS South/west_gradient005.tif", overwrite=TRUE)
+
+# Cloud cover from Wilson et al. (2016)
+clouds <- rast("GIS South/clouds_raw.tif")*0.01
+clouds <- resample(clouds, sample)
+writeRaster(clouds, "GIS South/clouds_005.tif", overwrite=TRUE)
+
+# Average emperature from max and min 
+tavg <- (rast("Data/Temperature/Tmin_ERA5_hr_1980_2020.tif") + rast("Data/Temperature/Tmin_ERA5_hr_1980_2020.tif"))/2
+writeRaster(tavg, "Data/Temperature/T2M_ERA5_hr_1980_2020.tif", overwrite=TRUE)
+
+# Climate class from Beck et al. (2019)
+climate_class <- rast("GIS South/climate_class_raw.tif")
+climate_class <- resample(climate_class, sample, method = "mode")
+climate_class[climate_class == 30] <- 29 # This produces a small bug
+
+climate_class <- as.factor(climate_class)
+levels(climate_class)<-c("Af",	 "Am",	"As",	 "Aw",	"BSh", "BSk",	"BWh", "BWk",	"Cfa", "Cfb",	
+                         "Cfc",	"Csa", "Csb",	"Csc",	"Cwa", "Cwb",	"Cwc", "Dfa",	"Dfb", "Dfc",
+                         "Dfd", "Dsa", "Dsb", "Dsc",  "Dwa", "Dwb",	"Dwc", "Dwd", "EF", "ET")
+
+writeRaster(climate_class, "GIS South/climate_class005.tif", overwrite=TRUE)
