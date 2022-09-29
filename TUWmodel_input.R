83e67b29f6bcd39df4c557377fec036bcf1dba83
rodaguayo <roodaguayo@gmail.com>
2022-09-29 18:43:36 +0200

    small bug fixes

--- TUWmodel_input.R
+++ TUWmodel_input.R
@@ -9,27 +9,24 @@ library("exactextractr")
 library("terra")
 library("sf")
 
-terra::gdalCache(17000)
 setwd("/home/rooda/Dropbox/Patagonia/")
-period  <- c(as.POSIXct("1980-01-01", tz = "UTC"), as.POSIXct("2019-12-31", tz = "UTC"))
+period  <- c(as.POSIXct("1987-01-01", tz = "UTC"), as.POSIXct("2019-12-31", tz = "UTC"))
 
 dem <- rast("GIS South/dem_patagonia3f.tif")
 dem <- aggregate(dem, fact=6, fun="mean")
 dem <- disagg(dem, fact=2, method = "bilinear") # avoid bug in elevation bands
 
-q_data  <- read.csv("Data/Streamflow/Data_Streamflow_v10_daily.csv")
-
-basins_int <- vect("GIS South/Basins_Patagonia83d.shp")
+basin_data <- read.csv("Data/Streamflow/Metadata_Streamflow_v10.csv")
+basins_int <- vect("GIS South/Basins_Patagonia83.shp")
 basins_int <- project(basins_int, crs(dem))
-basins_int <- basins_int[order(as.numeric(basins_int$gridcode)),]
 
 # PP and T2M: PMET v1.0; PET: GLEAM v3.6
 pp_pmet    <- rast("Data/Precipitation/PP_PMET_1980_2020d.nc")
 t2m_pmet   <- rast("Data/Temperature/Tavg_PMET_1980_2020d.nc")
 pet_gleam  <- rast("Data/Evapotranspiration/PET_GLEAM36a_1980_2021d.nc")
-time(pp_pmet)   <- as.POSIXct(time(pp_pmet))
-time(t2m_pmet)  <- as.POSIXct(time(t2m_pmet))
-time(pet_gleam) <- as.POSIXct(time(pet_gleam))
+terra::time(pp_pmet) <-   as.POSIXct(time(pp_pmet))
+terra::time(t2m_pmet) <-  as.POSIXct(time(t2m_pmet))
+terra::time(pet_gleam) <- as.POSIXct(time(pet_gleam))
 pp_pmet   <- pp_pmet[[time(pp_pmet)  >= period[1] & time(pp_pmet)  <= period[2]]]
 t2m_pmet  <- t2m_pmet[[time(t2m_pmet)  >= period[1] & time(t2m_pmet)  <= period[2]]]
 pet_gleam <- pet_gleam[[time(pet_gleam)  >= period[1] & time(pet_gleam)  <= period[2]]]
@@ -42,28 +39,27 @@ t2m_cr2met <- t2m_cr2met[[time(t2m_cr2met)  >= period[1] & time(t2m_cr2met)  <= 
 
 # PP: MSWEP v2.8; T2M: CR2MET v2.0; PET: GLEAM v3.6
 pp_mswep   <- rast("Data/Precipitation/PP_MSWEPv28_1979_2020d.nc")
-time(pp_mswep) <- as.POSIXct(time(pp_mswep))
+terra::time(pp_mswep) <- as.POSIXct(time(pp_mswep))
 pp_mswep  <- pp_mswep[[time(pp_mswep)  >= period[1] & time(pp_mswep)  <= period[2]]]
 
 # PP and T2M: ERA5d; PET: GLEAM v3.6
 pp_era5d   <- rast("Data/Precipitation/PP_ERA5_hr_1980_2020d.nc")
 t2m_era5d  <- rast("Data/Temperature/Tavg_ERA5_hr_1980_2020d.nc")
-time(pp_era5d)  <- as.POSIXct(time(pp_era5d))
-time(t2m_era5d) <- as.POSIXct(time(t2m_era5d))
+terra::time(pp_era5d)  <- as.POSIXct(time(pp_era5d))
+terra::time(t2m_era5d) <- as.POSIXct(time(t2m_era5d))
 pp_era5d   <- pp_era5d[[time(pp_era5d)  >= period[1] & time(pp_era5d)  <= period[2]]]
 t2m_era5d  <- t2m_era5d[[time(t2m_era5d)  >= period[1] & time(t2m_era5d)  <= period[2]]]
 
-data_area <- data.frame(matrix(0, length(basins_int), 7), row.names = names(basins_int$gridcode))
+data_area <- data.frame(matrix(0, length(basins_int), 7), row.names = names(basins_int$ID))
 colnames(data_area)<-c("area", "nbands",	"n1",	"n2",	"n3",	"n4",	"n5")
 
 for (i in 1:length(basins_int)) {
-  q_data_i <- subset(q_data, Date > period[1] & Date < period[2])[,i+1]
-  
-  if (sum(!is.na(q_data_i))/length(q_data_i) >= 0.5 & basins_int$Use[i] == 1){
+  if (basin_data$Modeled[i] == 1){
     
     elev_zones_i   <- elevationZones(x=basins_int[i,], dem=dem, max.zones = 5, min.elevZ = 300)
-    data_area[i,2] <- basins_int[i,]$area
-    data_area[i,3:((length(elev_zones_i$area))+2)]<-elev_zones_i$area
+    data_area[i,1] <- basins_int[i,]$Area_km2
+    data_area[i,2] <- length(elev_zones_i$area)
+    data_area[i,3:((length(elev_zones_i$area))+2)] <- elev_zones_i$area
     elev_zones_i <- as.polygons(elev_zones_i$zonesRaster, na.rm = TRUE, dissolve = TRUE)
     elev_zones_i <- st_as_sf(elev_zones_i)
     
