a0ae6ce7532fdf4915305f79a242bf558fbc2b75
Rodrigo Aguayo <roodguayo@gmail.com>
2023-05-22 20:24:33 -0400

    standard attribute names

--- Validation_PP.R
+++ Validation_PP.R
@@ -1,5 +1,5 @@
 # Code to replicate precipitation validation 
-# Developed by Rodrigo Aguayo (2020-2022)
+# Developed by Rodrigo Aguayo (2020-2023)
 
 rm(list=ls())
 cat("\014")  
@@ -9,11 +9,11 @@ library("terra")
 
 setwd("/home/rooda/Dropbox/Patagonia/Data/Precipitation/")
 period     <- c(as.POSIXct("1980-01-01"), as.POSIXct("2020-12-31"))
-attributes <- c("N", "ID", "Institution", "Latitude", "Longitude", "Altitude")
+attributes <- c("gauge_id", "gauge_name","institution", "gauge_lat", "gauge_lon", "gauge_alt")
 
 # Observations (location and data)
 pp_validation <- read.csv("PP_PMETobs_v10_metadata.csv")
-pp_shape      <- vect(pp_validation, geom=c("Longitude", "Latitude"), crs="epsg:4326")
+pp_shape      <- vect(pp_validation, geom=c("gauge_lon", "gauge_lat"), crs="epsg:4326")
 pp_obs        <- read.csv("PP_PMETobs_v10m.csv")
 pp_obs$Date   <- as.POSIXct(pp_obs$Date, tz= "UTC") # The date is the first column
 pp_obs        <- subset(pp_obs, Date >= period[1] &  Date <= period[2])
@@ -28,7 +28,7 @@ pp_stacks <- list(ERA5   = rast("PP_ERA5_1959_2021m.nc"),     # ERA5
                   CR2REG = rast("PP_REGCR2_1980_2015m.nc"),   # CR2REG 
                   CR2MET = rast("PP_CR2MET_1960_2021m.nc"),   # CR2MET v2.5
                   MSWEP  = rast("PP_MSWEPv28_1979_2020m.nc"), # MSWEP v2.8 
-                  W5D5   = rast("PP_W5D5_1979_2019m.nc"),     # W4D5 v2.0
+                  W5E5   = rast("PP_W5E5_1979_2019m.nc"),     # W5D5 v2.0
                   PMET   = rast("PP_PMET_1980_2020m.nc"))     # PMET v1.0 
 
 for (i in 1:length(pp_stacks)) {
--- Validation_T2M.R
+++ Validation_T2M.R
@@ -1,5 +1,5 @@
 # Code to replicate temperature validation 
-# Developed by Rodrigo Aguayo (2020-2022)
+# Developed by Rodrigo Aguayo (2020-2023)
 
 rm(list=ls())
 cat("\014")  
@@ -9,17 +9,17 @@ library("terra")
 
 setwd("/home/rooda/Dropbox/Patagonia/Data/Temperature/")
 period     <- c(as.POSIXct("1980-01-01"), as.POSIXct("2020-12-31"))
-attributes <- c("N", "ID", "Institution", "Latitude", "Longitude", "Altitude")
+attributes <- c("gauge_id", "gauge_name","institution", "gauge_lat", "gauge_lon", "gauge_alt")
 dem_lr     <- rast("/home/rooda/Dropbox/Patagonia/GIS South/dem_patagonia005.tif")
 
 # Observations (location and data)
 t2m_validation  <- read.csv("Tavg_PMETobs_v10_metadata.csv")
-t2m_shape <- vect(t2m_validation, geom=c("Longitude", "Latitude"), crs="epsg:4326")
+t2m_shape <- vect(t2m_validation, geom=c("gauge_lon", "gauge_lat"), crs="epsg:4326")
 t2m_shape$Altitude_lr <- extract(dem_lr, t2m_shape, method='simple')[,-1]
 
 t2m_obs      <- read.csv("Tavg_PMETobs_v10m.csv")
 Date <- as.POSIXct(t2m_obs$Date, tz= "UTC") #The date is the first column
-t2m_obs      <- sweep(t2m_obs[,-1], 2, (t2m_shape$Altitude_lr - t2m_shape$Altitude)*0.0065)
+t2m_obs      <- sweep(t2m_obs[,-1], 2, (t2m_shape$Altitude_lr - t2m_shape$gauge_alt)*0.0065)
 t2m_obs      <- cbind(Date, t2m_obs)
 t2m_validation <- subset(t2m_validation, select = attributes)
 
@@ -32,7 +32,7 @@ t2m_stacks <- list(ERA5  = rast("Tavg_ERA5_1959_2021m.nc"),     # ERA5
                   CR2REG = rast("Tavg_REGCR2_1980_2015m.nc"),   # CR2REG 
                   CR2MET = rast("Tavg_CR2MET_1960_2021m.nc"),   # CR2MET v2.5
                   MSWX   = rast("Tavg_MSWX_1979_2021m.nc"),     # MSWX 
-                  W5D5   = rast("Tavg_W5D5_1979_2019m.nc"),     # W5D5 v2.0
+                  W5E5   = rast("Tavg_W5E5_1979_2019m.nc"),     # W5D5 v2.0
                   PMET   = rast("Tavg_PMET_1980_2020m.nc"))     # PMET v1.0 
 
 for (i in 1:length(t2m_stacks)) {
