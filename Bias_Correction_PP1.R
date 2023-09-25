f061cbb08b281a18c78c9b9fc1c23b369d74ceed
Rodrigo Aguayo <roodaguayo@gmail.com>
2023-09-24 22:12:37 -0300

    added option for cross-validation (k-fold)

--- Bias_Correction_PP1.R
+++ Bias_Correction_PP1.R
@@ -14,24 +14,13 @@ library("doMC")
 setwd("/home/rooda/Dropbox/Patagonia/")
 terraOptions(memfrac=0.90)
 
+# use cross-validation (10-fold cv)? 
+CV = TRUE
+
 # 1. Data: observations and gridded product -------------------------------------------------------
 pp_stack_hr <- rast("Data/Precipitation/PP_ERA5_hr_1980_2020d.nc") # Gridded data
 period <- seq(as.Date("1980-01-01"), as.Date("2020-12-31"), by = "day")
 names(pp_stack_hr) <- time(pp_stack_hr)
-
-pp_shape <- read.csv("Data/Precipitation/PP_PMETobs_v10_metadata.csv") # Ground-base data
-pp_shape <- pp_shape[-c(1,2),] # bad fix
-pp_shape <- pp_shape[-178,] # bad fix
-pp_shape <- vect(pp_shape, geom=c("gauge_lon", "gauge_lat"), crs="epsg:4326")
-pp_shape <- crop(pp_shape, pp_stack_hr)
-
-pp_obs   <- read.csv("Data/Precipitation/PP_PMETobs_v10d.csv")
-pp_obs$Date <- as.Date(pp_obs$Date) # the date is the first column
-pp_obs   <- subset(pp_obs, Date >= min(period) & Date <= max(period))
-pp_obs   <- pp_obs[pp_shape$gauge_id]
-
-pp_sim   <- as.data.frame(t(extract(pp_stack_hr, pp_shape, method='simple')))[-1,]
-print ("1. Data: Ok")
 
 # list of covariables used in RF regressions
 covariates <- c(rast("GIS South/dist_coast005l.tif"),                # Distance to coast (log m)
@@ -46,99 +35,127 @@ names(covariates) <- c("distance_coast", "elevation", "t2m", "pp", "cloud_cover"
                        "west_gradient", "aspect", "aridity_index")
 covariates[is.infinite(covariates)] <- NA # bug in aridity_index
 
-rf_importance <- data.frame(matrix(ncol = 3 + nlyr(covariates), nrow = 0))
-colnames(rf_importance)  <- c("parameter", "var", "month", names(covariates))
-rf_importance[] <- lapply(rf_importance, as.character)
 
-rf_performance <- data.frame(matrix(ncol = 4, nrow = 0))
-QM_parameters  <- data.frame(matrix(ncol = 4, nrow = 0))
+for (cv in 1:10) {
 
-# validation set for spatial regression
-train_control <- rfeControl(functions = rfFuncs, # random forest
-                            method = "LGOCV",    # leave-group-out cross validation
-                            p = 0.90,            # the training percentage
-                            number = 100,        # number of repetitions
-                            allowParallel = TRUE)
-
-for (month in sprintf("%02d", 1:12)) {
+  pp_shape <- read.csv("Data/Precipitation/PP_PMETobs_v10_metadata.csv") # Ground-base data
+  pp_shape <- pp_shape[!pp_shape$gauge_id %in% c("X00000010", "X10360002", "X00085930"),] # bad fix
+  pp_shape <- vect(pp_shape, geom=c("gauge_lon", "gauge_lat"), crs="epsg:4326", keepgeom=TRUE)
+  pp_shape <- crop(pp_shape, pp_stack_hr)
   
-  print(paste0("Month ", month))
-  pp_obs_m <- subset(pp_obs, format(period,"%m") == month)
-  pp_sim_m <- subset(pp_sim, format(period,"%m") == month)
-  pp_stack_hr_m <- pp_stack_hr[[format(time(pp_stack_hr),"%m")  == month]]
+  # Cross-validation
+  set.seed(123)
+  pp_shape$fold <- createFolds(pp_shape$gauge_id, k = 10, list = F, returnTrain = F)
+  if (cv == 1) {write.csv(as.data.frame(pp_shape), "MS1 Results/CV_PP.csv", row.names = FALSE)}
+  if (CV) { pp_shape <- pp_shape[pp_shape$fold != cv,] } 
   
-  # 2. Parametric Quantile mapping ----------------------------------------------------------------
-  QM_parameters_m <- data.frame(matrix(0,ncol(pp_obs),6))
-  colnames(QM_parameters_m)<-c("var","month","a_linear","b_linear", "KGE_initial", "KGE_linear") 
+  pp_obs   <- read.csv("Data/Precipitation/PP_PMETobs_1950_2020_v10d.csv")
+  pp_obs$Date <- as.Date(pp_obs$Date) # the date is the first column
+  pp_obs   <- subset(pp_obs, Date >= min(period) & Date <= max(period))
+  pp_obs   <- pp_obs[pp_shape$gauge_id]
   
-  for (i in 1:length(pp_shape)) {
-    fit_ptf_linear  <- fitQmapPTF(pp_obs_m[,i], pp_sim_m[,i], transfun="linear",  wet.day=FALSE, cost="RSS")
-    pp_ptf_linear   <- doQmapPTF(pp_sim_m[,i], fit_ptf_linear)
+  pp_sim   <- as.data.frame(t(terra::extract(pp_stack_hr, pp_shape, method='simple')))[-1,]
+  
+  rf_importance <- data.frame(matrix(ncol = 3 + nlyr(covariates), nrow = 0))
+  colnames(rf_importance)  <- c("parameter", "var", "month", names(covariates))
+  rf_importance[] <- lapply(rf_importance, as.character)
+  
+  rf_performance <- data.frame(matrix(ncol = 4, nrow = 0))
+  QM_parameters  <- data.frame(matrix(ncol = 4, nrow = 0))
+  
+  # validation set for spatial regression
+  train_control <- rfeControl(functions = rfFuncs, # random forest
+                              method = "LGOCV",    # leave-group-out cross validation
+                              p = 0.90,            # the training percentage
+                              number = 100,        # number of repetitions
+                              allowParallel = TRUE)
+  
+  for (month in sprintf("%02d", 1:12)) {
     
-    QM_parameters_m$var    <- "PP"
-    QM_parameters_m$month  <- month
-    QM_parameters_m[i,3:4] <- fit_ptf_linear$par
-    QM_parameters_m[i,5:6] <- c(KGE(sim = pp_sim_m[,i],  obs=pp_obs_m[,i], method="2012", na.rm=TRUE), 
-                                KGE(sim = pp_ptf_linear, obs=pp_obs_m[,i], method="2012", na.rm=TRUE))
+    print(paste0("Month ", month))
+    pp_obs_m <- subset(pp_obs, format(period,"%m") == month)
+    pp_sim_m <- subset(pp_sim, format(period,"%m") == month)
+    pp_stack_hr_m <- pp_stack_hr[[format(time(pp_stack_hr),"%m")  == month]]
+    
+    # 2. Parametric Quantile mapping ----------------------------------------------------------------
+    QM_parameters_m <- data.frame(matrix(0,ncol(pp_obs),6))
+    colnames(QM_parameters_m)<-c("var","month","a_linear","b_linear", "KGE_initial", "KGE_linear") 
+    
+    for (i in 1:length(pp_shape)) {
+      fit_ptf_linear  <- fitQmapPTF(pp_obs_m[,i], pp_sim_m[,i], transfun="linear",  wet.day=FALSE, cost="RSS")
+      pp_ptf_linear   <- doQmapPTF(pp_sim_m[,i], fit_ptf_linear)
+      
+      QM_parameters_m$var    <- "PP"
+      QM_parameters_m$month  <- month
+      QM_parameters_m[i,3:4] <- fit_ptf_linear$par
+      QM_parameters_m[i,5:6] <- c(KGE(sim = pp_sim_m[,i],  obs=pp_obs_m[,i], method="2012", na.rm=TRUE), 
+                                  KGE(sim = pp_ptf_linear, obs=pp_obs_m[,i], method="2012", na.rm=TRUE))
+    }
+    
+    QM_parameters <- rbind(QM_parameters, QM_parameters_m[,1:4])
+    
+    ## 2.2. b linear parameter (a + pp *b) ----------------------------------------------------------
+    model_values <- data.frame(extract(covariates, pp_shape), b_linear = QM_parameters_m$b_linear)[,-1]
+  
+    registerDoMC(cores = 14)
+    set.seed(123)
+    rf_model <- rfe(x = model_values[names(covariates)], y = model_values$b_linear, 
+                    sizes = c(2:nlyr(covariates)), metric = "RMSE", 
+                    rfeControl = train_control, ntree = 500)
+    
+    rf_importante_i <- c(parameter = "b_linear", var = "PP", month = month, randomForest::importance(rf_model$fit)[,1])
+    rf_importance   <- dplyr::bind_rows(rf_importance, rf_importante_i)
+    performance_i   <- c(mean(rf_model$resample$RMSE), sd(rf_model$resample$RMSE),
+                        mean(rf_model$resample$Rsquared**0.5), sd(rf_model$resample$Rsquared**0.5))
+    rf_performance  <- rbind(rf_performance, c("pp_b", "pp", month, performance_i))
+    b_linear        <- predict(covariates[[predictors(rf_model)]], rf_model$fit, type='response', na.rm = T)
+    b_linear        <- focal(b_linear, w = focalMat(b_linear, 0.04, "Gauss"), pad = TRUE, na.rm=T)
+  
+    ## 2.1. a linear parameter (a + pp * b) ---------------------------------------------------------
+    model_values <- data.frame(extract(covariates, pp_shape), a_linear = QM_parameters_m$a_linear)[,-1]
+  
+    registerDoMC(cores = 14)
+    set.seed(123)
+    rf_model <- rfe(x = model_values[names(covariates)], y = model_values$a_linear, 
+                    sizes = c(1:nlyr(covariates)), metric = "RMSE",
+                    rfeControl = train_control, ntree = 500)
+    
+    rf_importante_i <- c(parameter = "a_linear", var = "PP", month = month, randomForest::importance(rf_model$fit)[,1])
+    rf_importance   <- dplyr::bind_rows(rf_importance, rf_importante_i)
+    performance_i   <- c(mean(rf_model$resample$RMSE), sd(rf_model$resample$RMSE),
+                         mean(rf_model$resample$Rsquared**0.5), sd(rf_model$resample$Rsquared**0.5))
+    rf_performance  <- rbind(rf_performance, c("pp_a", "pp", month, performance_i))
+    a_linear        <- predict(covariates[[predictors(rf_model)]], rf_model$fit, type='response', na.rm = T)
+    a_linear        <- focal(a_linear, w = focalMat(a_linear, 0.04, "Gauss"), pad = TRUE, na.rm=T)
+    
+    
+    ## 2.3. PQM correction for PMET -----------------------------------------------------------------
+    a_linear[covariates$elevation <= 1] <- NA # check
+    b_linear[covariates$elevation <= 1] <- NA
+    
+    pp_stack_hr_m <- a_linear + pp_stack_hr_m * b_linear # this is too slow and needs a lot of RAM
+    pp_stack_hr_m <- classify(pp_stack_hr_m, cbind(-Inf, 0, 0), right=FALSE)
+    
+    terra::time(pp_stack_hr_m) <- time(pp_stack_hr[[format(time(pp_stack_hr),"%m")  == month]])
+    
+    if (!CV) { path <- paste0("Data/Precipitation/PP_PMET_1980_2020d_", month, "_npc.nc")
+    } else {   path <-  paste0("/home/rooda/PMET_results/Precipitation/raw/PP_PMET_1980_2020d_", month, "_", cv, "_npc.nc")}
+
+    writeCDF(pp_stack_hr_m, path, overwrite=TRUE, varname="pp", unit="mm", zname="time", prec = "float",  shuffle = T)
+    
   }
-  
-  QM_parameters <- rbind(QM_parameters, QM_parameters_m[,1:4])
-  print ("2. PQM parameters: Ok")
-  
-  ## 2.2. b linear parameter (a + pp *b) ----------------------------------------------------------
-  model_values <- data.frame(extract(covariates, pp_shape), b_linear = QM_parameters$b_linear)[,-1]
-
-  registerDoMC(cores = 14)
-  set.seed(123)
-  rf_model <- rfe(x = model_values[names(covariates)], y = model_values$b_linear, 
-                  sizes = c(2:nlyr(covariates)), metric = "RMSE", 
-                  rfeControl = train_control, ntree = 500)
-  
-  rf_importante_i <- c(parameter = "b_linear", var = "PP", month = month, randomForest::importance(rf_model$fit)[,1])
-  rf_importance   <- dplyr::bind_rows(rf_importance, rf_importante_i)
-  performance_i   <- c(mean(rf_model$resample$RMSE), sd(rf_model$resample$RMSE),
-                      mean(rf_model$resample$Rsquared**0.5), sd(rf_model$resample$Rsquared**0.5))
-  rf_performance  <- rbind(rf_performance, c("pp_b", "pp", month, performance_i))
-  b_linear        <- predict(covariates[[predictors(rf_model)]], rf_model$fit, type='response', na.rm = T)
-  b_linear        <- focal(b_linear, w = focalMat(b_linear, 0.04, "Gauss"), pad = TRUE, na.rm=T)
-
-  ## 2.1. a linear parameter (a + pp * b) ---------------------------------------------------------
-  model_values <- data.frame(extract(covariates, pp_shape), a_linear = QM_parameters$a_linear)[,-1]
-
-  registerDoMC(cores = 14)
-  set.seed(123)
-  rf_model <- rfe(x = model_values[names(covariates)], y = model_values$a_linear, 
-                  sizes = c(1:nlyr(covariates)), metric = "RMSE",
-                  rfeControl = train_control, ntree = 500)
-  
-  rf_importante_i <- c(parameter = "a_linear", var = "PP", month = month, randomForest::importance(rf_model$fit)[,1])
-  rf_importance   <- dplyr::bind_rows(rf_importance, rf_importante_i)
-  performance_i   <- c(mean(rf_model$resample$RMSE), sd(rf_model$resample$RMSE),
-                       mean(rf_model$resample$Rsquared**0.5), sd(rf_model$resample$Rsquared**0.5))
-  rf_performance  <- rbind(rf_performance, c("pp_a", "pp", month, performance_i))
-  a_linear        <- predict(covariates[[predictors(rf_model)]], rf_model$fit, type='response', na.rm = T)
-  a_linear        <- focal(a_linear, w = focalMat(a_linear, 0.04, "Gauss"), pad = TRUE, na.rm=T)
-  
-  print ("2.2. a and b linear parameters: Ok")
-  
-  ## 2.3. PQM correction for PMET -----------------------------------------------------------------
-  a_linear[covariates$elevation <= 1] <- NA # check
-  b_linear[covariates$elevation <= 1] <- NA
-  
-  pp_stack_hr_m <- a_linear + pp_stack_hr_m * b_linear # this is too slow and needs a lot of RAM
-  pp_stack_hr_m <- classify(pp_stack_hr_m, cbind(-Inf, 0, 0), right=FALSE)
-  
-  print ("2.3. PQM applied to original netcdf: Ok")
-  
-  writeCDF(pp_stack_hr_m, paste0("Data/Precipitation/PP_PMET_1980_2020d_", month, "_npc.nc"),  
-           overwrite=TRUE, varname="pp", unit="mm", zname="time", prec = "float",  shuffle = T)
-
+  if (!CV) {break} 
+  print(paste0(cv,"-fold ok!"))
 }
 
-colnames(rf_importance)  <- c("parameter", "var", "month", names(covariates))
-write.csv(rf_importance, "MS1 Results/RF_PP_importance.csv", row.names = FALSE)
-
-colnames(rf_performance) <- c("parameter", "var", "month", "RMSE_mean", "RMSE_sd", "rPearson_mean", "rPearson_sd")
-write.csv(rf_performance, "MS1 Results/RF_PP_performance.csv", row.names = FALSE)
-
-write.csv(QM_parameters, "MS1 Results/PP_parameters.csv", row.names = FALSE)
+# save RF results: importance, performance and parameters
+if (!CV) {
+  
+  colnames(rf_importance)  <- c("parameter", "var", "month", names(covariates))
+  write.csv(rf_importance, "MS1 Results/RF_PP_importance.csv", row.names = FALSE)
+  
+  colnames(rf_performance) <- c("parameter", "var", "month", "RMSE_mean", "RMSE_sd", "rPearson_mean", "rPearson_sd")
+  write.csv(rf_performance, "MS1 Results/RF_PP_performance.csv", row.names = FALSE)
+  
+  write.csv(QM_parameters, "MS1 Results/PP_parameters.csv", row.names = FALSE)
+} 
--- Bias_Correction_PP2.R
+++ Bias_Correction_PP2.R
@@ -14,129 +14,142 @@ period <- seq(as.Date("1980-01-01"), as.Date("2020-12-31"), by = "day")
 setwd("/home/rooda/Dropbox/Patagonia/")
 terraOptions(memfrac=0.90)
 
-# 3. Precipitation factors ------------------------------------------------------------------------
-pp_stack_hr <- rast("Data/Precipitation/PP_PMET_1980_2020d_npc.nc") # Gridded data from step 1
-pp_stack_hr_m <- tapp(pp_stack_hr, strftime(time(pp_stack_hr), format="%Y"),  fun = sum, na.rm = TRUE)
-pp_stack_hr_m <- mean(pp_stack_hr_m)
+# use cross-validation (10-fold cv)? 
+CV = TRUE
 
-## 3.1 w factor for Budyko framework --------------------------------------------------------------
-basin_data        <- read.csv("Data/Streamflow/Q_PMETobs_v10_metadata.csv")
-basin_data$PET_PP <- round(basin_data$PET_BH/basin_data$PP_BH, 2)
-basin_data$ET_PP  <- round(basin_data$ET_BH/basin_data$PP_BH,  2)
-basin_data$ET_PP[basin_data$ET_PP >= 1] <- NA # strange case (only 1)
-basin_data$w      <- rep(NA, nrow(basin_data))
+for (cv in 1:10) {
 
-w_function <- function(PET_PP, ET_PP, w) 1 + PET_PP - ET_PP - (1+((PET_PP)^w))^(1/w)
-
-for (basin in 1:nrow(basin_data)) { 
-  if (!is.na(basin_data$ET_PP[basin])) { # Only basins with data in Chile (79 of 83)
-    basin_data$w[basin] <- uniroot(w_function, interval=c(0.8, 10), tol = 0.001, 
-                                   PET_PP=basin_data$PET_PP[basin], 
-                                   ET_PP=basin_data$ET_PP[basin])$root
-    basin_data$w[basin] <- round(basin_data$w[basin], 2)
+  # 3. Precipitation factors ------------------------------------------------------------------------
+  
+  # Gridded data from step 1
+  if (!CV) { path <- "Data/Precipitation/PP_PMETsim_1980_2020d_npc.nc"
+  } else {   path <- paste0("/home/rooda/PMET_results/Precipitation/PP_PMETsim_1980_2020d_npc_", cv, ".nc")} 
+  
+  pp_stack_hr <- rast(path)
+  pp_stack_hr_m <- tapp(pp_stack_hr, strftime(time(pp_stack_hr), format="%Y"),  fun = sum, na.rm = TRUE)
+  pp_stack_hr_m <- mean(pp_stack_hr_m)
+  
+  ## 3.1 w factor for Budyko framework --------------------------------------------------------------
+  basin_data        <- read.csv("Data/Streamflow/Q_PMETobs_v10_metadata.csv")
+  basin_data$PET_PP <- round(basin_data$PET_BH/basin_data$PP_BH, 2)
+  basin_data$ET_PP  <- round(basin_data$ET_BH/basin_data$PP_BH,  2)
+  basin_data$ET_PP[basin_data$ET_PP >= 1] <- NA # strange case (only 1)
+  basin_data$w      <- rep(NA, nrow(basin_data))
+  
+  w_function <- function(PET_PP, ET_PP, w) 1 + PET_PP - ET_PP - (1+((PET_PP)^w))^(1/w)
+  
+  for (basin in 1:nrow(basin_data)) { 
+    if (!is.na(basin_data$ET_PP[basin])) { # Only basins with data in Chile (79 of 83)
+      basin_data$w[basin] <- uniroot(w_function, interval=c(0.8, 10), tol = 0.001, 
+                                     PET_PP=basin_data$PET_PP[basin], 
+                                     ET_PP=basin_data$ET_PP[basin])$root
+      basin_data$w[basin] <- round(basin_data$w[basin], 2)
+    }
   }
-}
-basin_data$PET_PP <- NULL
-basin_data$ET_PP <- NULL
-print ("3.1 w factors: Ok")
-
-## 3.2 long-term precipitation  -------------------------------------------------------------------
-pp_function <- function(PP, Q, PET, dW, w) {
-  1 + (PET/(PP-dW)) - ((PP-Q-dW)/(PP-dW)) - (1+(PET/(PP-dW))^w)^(1/w)
-}
-basin_data$PP_TRUE <- rep(NA, nrow(basin_data))
-
-for (basin in 1:nrow(basin_data)) {
-  if (!is.na(basin_data$Qint_mm_y[basin])){ 
-    
-    basin_data$PP_TRUE[basin] <- uniroot(pp_function, tol = 0.001, 
-                                         interval=c( # problem with Qs less than 100
-                                           ifelse(basin_data$Qint_mm_y[basin] < 100, 50, 100), 
-                                           basin_data$Qint_mm_y[basin]+1000), 
-                                         Q   = basin_data$Qint_mm_y[basin], 
-                                         PET = basin_data$pet_mean_GLEAM[basin], 
-                                         dW  = basin_data$glacier_dhdt[basin],
-                                         w   = median(basin_data$w, na.rm = T))$root
-    basin_data$PP_TRUE[basin] <- round(basin_data$PP_TRUE[basin], 0)
+  basin_data$PET_PP <- NULL
+  basin_data$ET_PP <- NULL
+  print ("3.1 w factors: Ok")
+  
+  ## 3.2 long-term precipitation  -------------------------------------------------------------------
+  pp_function <- function(PP, Q, PET, dW, w) {
+    1 + (PET/(PP-dW)) - ((PP-Q-dW)/(PP-dW)) - (1+(PET/(PP-dW))^w)^(1/w)
   }
-}
-
-print ("3.2 long-term precipitation: Ok")
+  basin_data$PP_TRUE <- rep(NA, nrow(basin_data))
+  
+  for (basin in 1:nrow(basin_data)) {
+    if (!is.na(basin_data$Qint_mm_y[basin])){ 
+      
+      basin_data$PP_TRUE[basin] <- uniroot(pp_function, tol = 0.001, 
+                                           interval=c( # problem with Qs less than 100
+                                             ifelse(basin_data$Qint_mm_y[basin] < 100, 50, 100), 
+                                             basin_data$Qint_mm_y[basin]+1000), 
+                                           Q   = basin_data$Qint_mm_y[basin], 
+                                           PET = basin_data$pet_mean_GLEAM[basin], 
+                                           dW  = basin_data$glacier_dhdt[basin],
+                                           w   = median(basin_data$w, na.rm = T))$root
+      basin_data$PP_TRUE[basin] <- round(basin_data$PP_TRUE[basin], 0)
+    }
+  }
+  
+  print ("3.2 long-term precipitation: Ok")
 
 ## 3.3 PP factor for each basin centroid ----------------------------------------------------------
-basins_shp_int             <- sf::st_read("GIS South/Basins_PMET_v10_int.shp")
-basin_data$PP_PMET_int     <- exact_extract(pp_stack_hr_m, basins_shp_int, "mean")
-basin_data$BF_PMET         <- round(basin_data$PP_TRUE/basin_data$PP_PMET_int, 3)
-basin_data$BF_PMET[basin_data$BF_PMET > 2.5] <- NA # remove outliers
-basins_shp_int$BF_PMET     <- basin_data$BF_PMET
-basins_shp_int             <- subset(basins_shp_int, !is.na(basin_data$BF_PMET))
-basins_shp_int             <- centroids(vect(basins_shp_int))
 
-basin_data$PP_PMET_int <- NULL
-write.csv(basin_data, "Data/Streamflow/Q_PMETobs_v10_metadata.csv", row.names = FALSE)
-print ("3.3 PP factors: Ok")
+  basins_shp_int         <- sf::st_read("GIS South/Basins_PMETobs_v10_int.shp")
+  basin_data$PP_PMET_int <- exact_extract(pp_stack_hr_m, basins_shp_int, "mean")
+  basin_data$BF_PMET     <- round(basin_data$PP_TRUE/basin_data$PP_PMET_int, 3)
+  basin_data$BF_PMET[basin_data$BF_PMET > 2.25]  <- NA # remove outliers
+  basins_shp_int$BF_PMET     <- basin_data$BF_PMET
+  
+  ### Cross-validation
+  set.seed(123)
+  basin_data$fold <- createFolds(basin_data$gauge_id, k = 10, list = F, returnTrain = F)
+  basins_shp_int$fold <- basin_data$fold
+  
+  if (CV) { basin_data     <- basin_data[basin_data$fold != cv,] 
+            basins_shp_int <- basins_shp_int[basins_shp_int$fold != cv,]} 
+  
+  basins_shp_int             <- subset(basins_shp_int, !is.na(basin_data$BF_PMET))
+  basins_shp_int             <- centroids(vect(basins_shp_int))
+  
+  basin_data$PP_PMET_int <- NULL
+  if (!CV) { write.csv(basin_data, "Data/Streamflow/Q_PMETobs_v10_metadata.csv", row.names = F) } 
 
-## 3.4 random forest for PP factors ---------------------------------------------------------------
-covariates <- c(rast("GIS South/dist_coast005l.tif"),                # Distance to coast (log m)
-                rast("GIS South/dem_patagonia005.tif"),              # Altitude (masl)
-                rast("Data/Temperature/Tavg_ERA5_hr_1980_2020.tif"), # Annual temperature (degC)
-                rast("Data/Precipitation/PP_ERA5_hr_1980_2020.tif"), # Annual precipitation (mm)
-                rast("GIS South/clouds_005.tif"),                    # Cloud cover (%)
-                rast("GIS South/west_gradient005.tif"),              # West gradient
-                rast("GIS South/aspect_dem005.tif"),                 # Aspect
-                rast("GIS South/aridity_index_ERA5_hr_005.tif"))     # Aridity index
-names(covariates) <- c("distance_coast", "elevation", "t2m", "pp", "cloud_cover", 
-                       "west_gradient", "aspect", "aridity_index")
-covariates[is.infinite(covariates)] <- NA # bug in aridity_index
+  print ("3.3 PP factors: Ok")
+  
+  ## 3.4 random forest for PP factors ---------------------------------------------------------------
+  covariates <- c(rast("GIS South/dist_coast005l.tif"),                # Distance to coast (log m)
+                  rast("GIS South/dem_patagonia005.tif"),              # Altitude (masl)
+                  rast("Data/Temperature/Tavg_ERA5_hr_1980_2020.tif"), # Annual temperature (degC)
+                  rast("Data/Precipitation/PP_ERA5_hr_1980_2020.tif"), # Annual precipitation (mm)
+                  rast("GIS South/clouds_005.tif"),                    # Cloud cover (%)
+                  rast("GIS South/west_gradient005.tif"),              # West gradient
+                  rast("GIS South/aspect_dem005.tif"),                 # Aspect
+                  rast("GIS South/aridity_index_ERA5_hr_005.tif"))     # Aridity index
+  names(covariates) <- c("distance_coast", "elevation", "t2m", "pp", "cloud_cover", 
+                         "west_gradient", "aspect", "aridity_index")
+  covariates[is.infinite(covariates)] <- NA # bug in aridity_index
+  
+  train_control <- rfeControl(functions = rfFuncs, # random forest
+                              method = "LGOCV",    # leave-group-out cross validation
+                              p = 0.90,            # the training percentage
+                              number = 100,        # number of repetitions
+                              allowParallel = TRUE)
+  
+  model_values  <- data.frame(terra::extract(covariates, basins_shp_int), bias_factor = basins_shp_int$BF_PMET)[,-1]
+  model_values  <- model_values[-nrow(model_values),] # remove last basin
+  
+  set.seed(123)
+  rf_model <- rfe(x = model_values[names(covariates)], y = model_values$bias_factor, 
+                  sizes = c(2:nlyr(covariates)), metric = "RMSE", 
+                  rfeControl = train_control, ntree = 500)
+  
+  rf_importance   <- c(parameter = "pp_factor", var = "PP", month = "00", randomForest::importance(rf_model$fit)[,1])
+  rf_performance  <- c(parameter = "pp_factor", var = "pp", month = "00", 
+                       RMSE_mean = mean(rf_model$resample$RMSE), 
+                       RMSE_sd = sd(rf_model$resample$RMSE), 
+                       rPearson_mean = mean(rf_model$resample$Rsquared**0.5), 
+                       rPearson_sd = sd(rf_model$resample$Rsquared**0.5))
+  
+  bias_factor   <- terra::predict(covariates[[predictors(rf_model)]], rf_model$fit, type='response', na.rm = T)
+  bias_factor  <- focal(bias_factor, w = focalMat(pp_stack_hr_m, 0.04, "Gauss"), pad = TRUE)
+  bias_factor[bias_factor <= 1] <- 1
+  
+  if (!CV) { path <-  "MS1 Results/Bias_Factor_PP.tif" 
+  } else {   path <-  paste0("/home/rooda/PMET_results/Precipitation/Bias_Factor_PP_", cv, ".tif")}
+  writeRaster(bias_factor, path, overwrite = TRUE)
+  
+  print ("3.4 RF regression: Ok")
 
-train_control <- rfeControl(functions = rfFuncs, # random forest
-                            method = "LGOCV",    # leave-group-out cross validation
-                            p = 0.90,            # the training percentage
-                            number = 100,        # number of repetitions
-                            allowParallel = TRUE)
+  if (!CV) {break} 
+  print(paste0(cv,"-fold ok!"))
+  
+}
 
-model_values  <- data.frame(extract(covariates, basins_shp_int), bias_factor = basins_shp_int$BF_PMET)[,-1]
-model_values  <- model_values[-nrow(model_values),] # remove last basin
-
-set.seed(123)
-rf_model <- rfe(x = model_values[names(covariates)], y = model_values$bias_factor, 
-                sizes = c(2:nlyr(covariates)), metric = "RMSE", 
-                rfeControl = train_control, ntree = 500)
-
-rf_importance   <- c(parameter = "pp_factor", var = "PP", month = "00", randomForest::importance(rf_model$fit)[,1])
-rf_performance  <- c(parameter = "pp_factor", var = "pp", month = "00", 
-                     RMSE_mean = mean(rf_model$resample$RMSE), 
-                     RMSE_sd = sd(rf_model$resample$RMSE), 
-                     rPearson_mean = mean(rf_model$resample$Rsquared**0.5), 
-                     rPearson_sd = sd(rf_model$resample$Rsquared**0.5))
-
-write.csv(t(as.data.frame(rf_importance)), "MS1 Results/RF_PP_factor_importance.csv", row.names = FALSE)
-write.csv(rf_performance, "MS1 Results/RF_PP_factor_performance.csv", row.names = FALSE)
-
-bias_factor   <- terra::predict(covariates[[predictors(rf_model)]], rf_model$fit, type='response', na.rm = T)
-#bias_factor  <- focal(bias_factor, w = focalMat(pp_stack_hr_m, 0.04, "Gauss"), pad = TRUE)
-bias_factor[bias_factor <= 1] <- 1
-writeRaster(bias_factor, "MS1 Results/Bias_Factor_PP.tif", overwrite = TRUE)
-
-pp_pmet   <- pp_stack_hr * bias_factor
-pp_pmet   <- round(pp_pmet, 0)
-print ("3.4 RF regression: Ok")
-
-# 4. Saving and resampling files ------------------------------------------------------------------
-pp_pmet_m <- tapp(pp_pmet,   strftime(time(pp_pmet), format="%Y-%m"), fun = sum, na.rm = TRUE)
-terra::time(pp_pmet_m) <- seq(from = min(period), to = max(period), by = "month")
-pp_pmet_y <- tapp(pp_pmet_m, strftime(time(pp_pmet_m), format="%Y"),  fun = sum, na.rm = TRUE)
-pp_pmet_y <- mean(pp_pmet_y)
-
-writeCDF(pp_pmet, "Data/Precipitation/PP_PMET_1980_2020d.nc",  overwrite=TRUE, 
-         varname="pp", unit="mm", zname="time", prec = "float",  shuffle = T)
-
-writeCDF(pp_pmet_m, "Data/Precipitation/PP_PMET_1980_2020m.nc",  overwrite=TRUE, 
-         varname="pp", unit="mm", zname="time", compression = 9, prec = "float")
-
-writeRaster(pp_pmet_y, "Data/Precipitation/PP_PMET_1980_2020.tif", overwrite=TRUE)
-print (paste0("4. pp files: Ok"))
-
+if (!CV) {
+  write.csv(t(as.data.frame(rf_importance)), "MS1 Results/RF_PP_factor_importance.csv", row.names = FALSE)
+  write.csv(rf_performance, "MS1 Results/RF_PP_factor_performance.csv", row.names = FALSE)
+}
 
 # 5. TODOs ---------------------------------------------------------------------------------------
 
